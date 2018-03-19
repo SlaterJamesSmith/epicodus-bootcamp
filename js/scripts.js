185e2a2fdbb9b32a3495f02a2501a8647e5b7b01
Mike Chu and Matthew Dowdney <mike@>
2018-03-19 16:05:16 -0700

    Overhaul cipher functions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,41 @@
+$(document).ready(function() {
+  $(".img1").click(function() {
+    alert(userString);
+  });
+
+  $(".img2").click(function() {
+    reverseString(fourthString);
+  });
+});
+
+
 var userString = prompt("Please enter a complete sentence.");
+var capsString;
+alert("You entered: \"" + userString + "\"");
 
-var newString = userString.replace(/^./, userString.charAt(userString.length - 1).toUpperCase()).replace(/.$/, userString.charAt(0).toUpperCase());
+function capitalizeEnds(string) {
+  capsString = userString.replace(/^./, userString.charAt(0).toUpperCase()).replace(/.$/, userString.charAt(userString.length - 1).toUpperCase());
+};
 
-alert(newString);
+capitalizeEnds(userString);
+
+var capsFirst = capsString.charAt(0);
+var capsLast = capsString.charAt(capsString.length - 1);
+
+var thirdString = userString + capsLast + capsFirst
+var fourthString;
+
+function scramble(string) {
+  var charOffset = userString.charAt(userString.length / 2);
+  fourthString = charOffset + thirdString;
+};
+
+scramble(userString);
+
+function reverseString(string) {
+  alert("Your garbled message is: " + string.split("").reverse().join(""));
+};
+reverseString(fourthString);
+
+
+//var newString = userString.replace(/^./, userString.charAt(userString.length - 1).toUpperCase()).replace(/.$/, userString.charAt(0).toUpperCase());
