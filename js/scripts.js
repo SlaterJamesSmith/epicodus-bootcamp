cb6d6e38115cd906e9d207a70636f2cb6cd0774b
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-03-19 23:15:55 -0700

    Clean up unnecessary content and code.

--- js/scripts.js
+++ js/scripts.js
@@ -29,23 +29,3 @@ function cipher(string) {
   cipherMessage = userMessage.charAt(parseInt(userMessage.length / 2)) + cipherMessage;
   cipherMessage = cipherMessage.split("").reverse().join("");
 };
-
-// capitalizeEnds(userString);
-//
-// var capsFirst = capsString.charAt(0);
-// var capsLast = capsString.charAt(capsString.length - 1);
-//
-// var thirdString = userString + capsLast + capsFirst
-// var fourthString;
-//
-// function scramble(string) {
-//   var charOffset = userString.charAt(userString.length / 2);
-//   fourthString = charOffset + thirdString;
-// };
-//
-// scramble(userString);
-//
-// function reverseString(string) {
-//   alert("Your garbled message is: " + string.split("").reverse().join(""));
-// };
-// reverseString(fourthString);
