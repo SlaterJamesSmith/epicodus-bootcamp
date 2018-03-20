baa5ed64d850fe131b1191ce94fb08237d21dad9
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-03-19 22:44:11 -0700

    Recreate prompt mechanic and create message checking function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,45 @@
 $(document).ready(function() {
-  $(".").click(function() {
+  $(".activate").click(function() {
     userMessage = prompt("Enter a message for the cipher:");
+    messageCheck();
   });
 
-  $(".img2").click(function() {
-    reverseString(fourthString);
+  $(".monitor").click(function() {
+    alert(userMessage);
   });
 });
 
-var userMessage = "No Message Input."
+var userMessage = "Error: No Message Input."
 
-var capsString;
-
-alert("You entered: \"" + userString + "\"");
-
-function capitalizeEnds(string) {
-  capsString = userString.replace(/^./, userString.charAt(0).toUpperCase()).replace(/.$/, userString.charAt(userString.length - 1).toUpperCase());
+function messageCheck() {
+  if (userMessage === null || userMessage === "" || userMessage === undefined) {
+    userMessage = "Error: No Message Input.";
+    alert(userMessage);
+  } else {
+    alert("Message Sent.");
+  };
 };
-
-capitalizeEnds(userString);
-
-var capsFirst = capsString.charAt(0);
-var capsLast = capsString.charAt(capsString.length - 1);
-
-var thirdString = userString + capsLast + capsFirst
-var fourthString;
-
-function scramble(string) {
-  var charOffset = userString.charAt(userString.length / 2);
-  fourthString = charOffset + thirdString;
-};
-
-scramble(userString);
-
-function reverseString(string) {
-  alert("Your garbled message is: " + string.split("").reverse().join(""));
-};
-reverseString(fourthString);
-
-
-//var newString = userString.replace(/^./, userString.charAt(userString.length - 1).toUpperCase()).replace(/.$/, userString.charAt(0).toUpperCase());
+//
+// function capitalizeEnds(string) {
+//   capsString = userString.replace(/^./, userString.charAt(0).toUpperCase()).replace(/.$/, userString.charAt(userString.length - 1).toUpperCase());
+// };
+//
+// capitalizeEnds(userString);
+//
+// var capsFirst = capsString.charAt(0);
+// var capsLast = capsString.charAt(capsString.length - 1);
+//
+// var thirdString = userString + capsLast + capsFirst
+// var fourthString;
+//
+// function scramble(string) {
+//   var charOffset = userString.charAt(userString.length / 2);
+//   fourthString = charOffset + thirdString;
+// };
+//
+// scramble(userString);
+//
+// function reverseString(string) {
+//   alert("Your garbled message is: " + string.split("").reverse().join(""));
+// };
+// reverseString(fourthString);
