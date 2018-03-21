fabfaf30c22de7aa9a678d27d009cd05fa6ab30f
Brenda Franco and Mike Chu <brenda@>
2018-03-21 09:35:34 -0700

    Add javascript to pull and display form input.

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    //Business logic to gather form input
+    var person1Input = $("input#person1").val();
+    var person2Input = $("input#person2").val();
+    var animalInput = $("input#animal").val();
+    var exclamationInput = $("input#exclamation").val();
+    var verbInput = $("input#verb").val();
+    var nounInput = $("input#noun").val();
+
+    //User logic to display text in story
+    $(".person1").append(person1Input);
+    $(".person2").append(person2Input);
+    $(".animal").append(animalInput);
+    $(".exclamation").append(exclamationInput);
+    $(".verb").append(verbInput);
+    $(".noun").append(nounInput);
+
+    $("#story").show();
+
+    event.preventDefault();
+  });
+
+});
