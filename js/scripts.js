158ff711e903a144bc0f264d39bc2eb543245639
Brenda Franco and Mike Chu <brenda@>
2018-03-21 11:31:53 -0700

    Add form and submit action for addition function.

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,16 @@ var add = function(number1, number2) {
 };
 
 // User Interface Logic
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
 
-alert(add(number1, number2));
+    var number1 = parseInt($("input#add1").val());
+    var number2 = parseInt($("input#add2").val());
+    var result = add(number1, number2);
+
+    $("#addresult").text(result);
+
+  });
+
+});
