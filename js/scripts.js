5ea10017d9c2156e47a8367361ab6d258e9f3f0b
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-03-21 22:48:15 -0700

    Restructure HTML elements for calculator interface.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-var number1 = 0;
-var number2 = 0;
+var number1 = "0";
+var number2 = "0";
 var operatorSelection;
 
 function add(number1, number2) {
@@ -19,8 +19,23 @@ function multiply(number1, number2) {
 };
 
 $(document).ready(function() {
+  $("#clear-all").click(function() {
+    number1 = "0";
+    number2 = "0";
+    operatorSelection = "none";
+    $("#display").text("0");
+  });
+
   $("#key1").click(function() {
-    number1 = 1
+    if ($("#display").html() === "0") {
+      $("#display").text("1");
+    } else {
+        $("#display").append("1");
+      };
+  });
+
+  $("#key2").click(function() {
+    number1 = "2"
     if ($("#display").html() === "0") {
       $("#display").text("1");} else {
         $("#display").append("1");
