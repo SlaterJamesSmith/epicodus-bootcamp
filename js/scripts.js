05772a396a297bfb2b1dd6b5c11e36efabae30ee
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-03-21 22:19:02 -0700

    Implement display functionality for key 1.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+var number1 = 0;
+var number2 = 0;
+var operatorSelection;
+
 function add(number1, number2) {
   return number1 + number2;
 };
@@ -13,3 +17,15 @@ function divide(number1, number2) {
 function multiply(number1, number2) {
   return number1 * number2;
 };
+
+$(document).ready(function() {
+  $("#key1").click(function() {
+    number1 = 1
+    if ($("#display").html() === "0") {
+      $("#display").text("1");} else {
+        $("#display").append("1");
+      };
+  });
+
+
+});
