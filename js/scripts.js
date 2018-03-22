bcf5034cfc13d3bdc775290086975604c824d44a
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-03-21 21:30:44 -0700

    Added jQuery and Bootstrap to project.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,15 @@
-var add = function(number1, number2) {
+function add(number1, number2) {
   return number1 + number2;
 };
 
-var subtract = function(number1, number2) {
+function subtract(number1, number2) {
   return number1 - number2;
 };
 
-var divide = function(number1, number2) {
+function divide(number1, number2) {
   return number1 / number2;
 };
 
-var multiply = function(number1, number2) {
+function multiply(number1, number2) {
   return number1 * number2;
 };
-
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = subtract(number1, number2);
-alert(result);
