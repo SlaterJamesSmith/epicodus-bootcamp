096966d93dc10b5440ade038af05a760dd205d3b
Mike Chu and Ted Leary <ted@>
2018-03-22 09:06:19 -0700

    Prepare Animal Wiki project for repository merge.

---
+++ js/animal-wiki-scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+  $(".chimp").click(function() {
+    $(".invis").hide();
+    $(".chimpinfo").show();
+
+  });
+
+  $(".lion").click(function() {
+    $(".invis").hide();
+    $(".lioninfo").show();
+
+  });
+
+  $(".bear").click(function() {
+    $(".invis").hide();
+    $(".bearinfo").show();
+
+  });
+
+});
