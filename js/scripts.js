44059e52950164f76344730e13b31e9f172fa3a4
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-03-23 13:22:19 -0700

    Add form reset behavior when retaking survey.

--- js/scripts.js
+++ js/scripts.js
@@ -10,10 +10,6 @@ $(document).ready(function() {
     var aptitudeCode = $("input:radio[name=aptitude1]:checked").val() + $("input:radio[name=aptitude2]:checked").val();
     var attitudeCode = $("input:radio[name=attitude1]:checked").val();
     var interestCode = $("input:radio[name=interest1]:checked").val() + $("input:radio[name=interest2]:checked").val() + $("input:radio[name=interest3]:checked").val();
-
-    console.log(aptitudeCode);
-    console.log(attitudeCode);
-    console.log(interestCode);
     //
     // var aptitudeScore =
     // var attitudeScore = ;
@@ -39,6 +35,6 @@ $(document).ready(function() {
   $("#retake-survey").click(function() {
     $("form").show();
     $(".no-display").hide();
-
+    $("form")[0].reset();
   });
 });
