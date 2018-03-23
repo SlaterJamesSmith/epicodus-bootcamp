02a32fa22d70abaa14087ca5edcd63e365959e09
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-03-23 15:06:02 -0700

    Add content to Keep Exploring section.

--- js/scripts.js
+++ js/scripts.js
@@ -16,40 +16,30 @@ $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
 
-    $("#input-name").text($("#firstname").val().slice(0,1).toUpperCase() + $("#firstname").val().slice(1,$("#firstname").val().length).toLowerCase());
+    $(".input-name").text($("#firstname").val().slice(0,1).toUpperCase() + $("#firstname").val().slice(1,$("#firstname").val().length).toLowerCase());
 
     var aptitudeCode = $("input:radio[name=aptitude1]:checked").val() + $("input:radio[name=aptitude2]:checked").val();
     var attitudeCode = $("input:radio[name=attitude1]:checked").val();
     var interestCode = $("input:radio[name=interest1]:checked").val() + $("input:radio[name=interest2]:checked").val() + $("input:radio[name=interest3]:checked").val();
 
-    console.log("TP0 AptCode: " + aptitudeCode);
-    console.log("TP1 AttCode: " + attitudeCode);
-    console.log("TP2 IntCode: " + interestCode);
-
     var aptitudeScore = tallyScore(aptitudeCode.slice(0,1)) + tallyScore(aptitudeCode.slice(1,2));
     var attitudeScore = tallyScore(attitudeCode);
     var interestScore = tallyScore(interestCode.slice(0,1)) + tallyScore(interestCode.slice(1,2)) + tallyScore(interestCode.slice(2,3));
 
-    console.log("TP10 AptScore: " + aptitudeScore);
-    console.log("TP11 AttScore: " + attitudeScore);
-    console.log("TP12 IntScore: " + interestScore);
-
-    //
-    // if () {
-    //
-    // } else if () {
-    //
-    // } else if () {
-    //
-    // } else if () {
-    //
-    // } else {
-    //
-    // };
-
     $("form").hide();
-    $("#thankyou").show();
+    $("#thankyou").fadeIn();
 
+    // if () {
+    //   $("#result1").fadeIn();
+    // } else if () {
+    //   $("#result2").fadeIn();
+    // } else if () {
+    //   $("#result3").fadeIn();
+    // } else if () {
+    //   $("#result4").fadeIn();
+    // } else {
+    //   $("#result5").fadeIn();
+    // };
   });
 
   $("#retake-survey").click(function() {
