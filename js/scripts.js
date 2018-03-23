0a3cdef2989cb907c5a8c9df9641d63505271b35
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-03-23 14:08:03 -0700

    Create scoring function on trait codes and introduce temporary data testpoints.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,15 @@
 // BUSINESS LOGIC
+function tallyScore(traitCode) {
+  if (traitCode === "A") {
+    return 0;
+  } else if (traitCode === "B") {
+    return 1;
+  } else if (traitCode === "C") {
+    return 2;
+  } else {
+    return;
+  };
+};
 
 // USER INTERFACE LOGIC
 $(document).ready(function() {
@@ -10,10 +21,19 @@ $(document).ready(function() {
     var aptitudeCode = $("input:radio[name=aptitude1]:checked").val() + $("input:radio[name=aptitude2]:checked").val();
     var attitudeCode = $("input:radio[name=attitude1]:checked").val();
     var interestCode = $("input:radio[name=interest1]:checked").val() + $("input:radio[name=interest2]:checked").val() + $("input:radio[name=interest3]:checked").val();
-    //
-    // var aptitudeScore =
-    // var attitudeScore = ;
-    // var interestScore = ;
+
+    console.log("AptCode: " + aptitudeCode);
+    console.log("AttCode: " + attitudeCode);
+    console.log("IntCode: " + interestCode);
+
+    var aptitudeScore;
+    var attitudeScore = tallyScore(attitudeCode);
+    var interestScore;
+
+    console.log("AptScore: " + aptitudeScore);
+    console.log("AttScore: " + attitudeScore);
+    console.log("IntScore: " + interestScore);
+
     //
     // if () {
     //
