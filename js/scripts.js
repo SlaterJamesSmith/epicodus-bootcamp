3fa8c9aee7348336a85b5238cd677732fbaa0beb
Jesse Drebin and Mike Chu <jesse@>
2018-03-26 11:34:02 -0700

    Add HTML form and generate array for user inputs.

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+
+    var groceryForm = ["item1", "item2", "item3", "item4", "item5"];
+    var groceryList = groceryForm.map(function(item) {
+      return $("#" + item).val().toUpperCase();
+    });
+
+    groceryList.sort();
+    alert(groceryList);
+    $("form").hide();
+
+  });
+
+  // $(".addItem").click(function() {
+  //   $("form").append('<div class=\"form-group\"><label>Item</label><input type=\"text\"></div>');
+  //
+  // });
+
+
+});
