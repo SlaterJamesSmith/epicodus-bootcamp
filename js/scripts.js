71fdce8ef105d6f2eb6d25df63140c3e4f51ea14
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-03-26 21:03:27 -0700

    Update javascript to save user inputs in individual panels.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,26 @@
-var favoriteThings = [];
-
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
 
-    favoriteThings.push($("#name").val());
+    var favoriteThings = [];
+
     favoriteThings.push($("#beverage").val());
     favoriteThings.push($("#pet").val());
     favoriteThings.push($("#food").val());
 
-    $("ul").append("<li>" + favoriteThings[2] + "</li>");
-    $("ul").append("<li>" + favoriteThings[1] + "</li>");
-    $("ul").append("<li>" + favoriteThings[3] + "</li>");
+    $("#results").prepend('<div class=\"record well\"><h2 class=\"nameBlank\">\'s<br>Favorite Things</h2><ul></ul></div>');
+
+    $("#results").children("div").first().click(function() {
+      $(this).remove();
+    });
+
+    $("#results .record").children(".nameBlank").first().prepend($("#name").val());
+
+    favoriteThings.forEach(function(item) {
+      $("#results .record").children("ul").first().append("<li>" + item + "</li>");
+    });
+
+    $("#results").show();
 
   });
 
