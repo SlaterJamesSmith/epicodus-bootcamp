7938b6715dcc75a58e4e5047b5c619f369f0b5ff
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-03-26 22:09:37 -0700

    Update javascript with array sort and click event handler.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
-  var favoriteFlavors = ["vanilla", "chocolate", "strawberry", "neopolitan"]
-  favoriteFlavors.forEach(function(flavor) {
-    $(".icecream").append("<p>"+ flavor + "</p>")
+  var favoriteFlavors = ["vanilla", "chocolate", "strawberry", "neopolitan", "black cherry", "mocha"];
+
+  favoriteFlavors.sort();
+
+  $("#ice-cream button").click(function() {
+    favoriteFlavors.forEach(function(flavor) {
+      $("#ice-cream ul").append("<li>"+ flavor.toUpperCase() + "</li>")
+    });
   });
 
 });
