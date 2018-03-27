4eecb7a0471ab784e2fd93ffc80e9b61850dd5d5
Mike Chu and Pablo Barreyro <pablo@>
2018-03-27 10:12:56 -0700

    Add looping logic to replace vowels in string input to Word Puzzle.

---
+++ js/puzzle_scripts.js
@@ -0,0 +1,20 @@
+var vowels = ["a","e","i","o","u"];
+
+$(document).ready(function(){
+  var inputStringAsArray = prompt("enter string").split("");
+  console.log(inputStringAsArray);
+  var outputStringArray = [];
+
+  for (var index = 0; index<inputStringAsArray.length; index+=1) {
+    var pushValue = inputStringAsArray[index];
+
+    vowels.forEach(function(vowel){
+      if (pushValue === vowel) {
+        pushValue = "-";
+      };
+    });
+
+    outputStringArray.push(pushValue);
+  }
+  console.log(outputStringArray);
+});
