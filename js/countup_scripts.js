f92b9f5466f476393132331fb1961188d879f7e5
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-03-27 19:20:13 -0700

    Update Count Project HTML and javascript with fix for over-count error.

--- js/countup_scripts.js
+++ js/countup_scripts.js
@@ -1,9 +1,8 @@
 var countOutputs = [];
-var currentCount;
 
 function countUp(number1, number2) {
   countOutputs.push(number1);
-  for (currentCount = number1; currentCount < number2 ; currentCount) {
+  for (currentCount = number1; currentCount + number1 < number2 ; currentCount) {
     currentCount += number1;
     countOutputs.push(currentCount);
   };
@@ -11,15 +10,19 @@ function countUp(number1, number2) {
 
 function countDown(number1, number2) {
   countOutputs.push(number1);
-  for (currentCount = number1; currentCount > number2 ; currentCount) {
+  for (currentCount = number1; currentCount + number1 > number2 ; currentCount) {
     currentCount += number1;
     countOutputs.push(currentCount);
   };
 };
 
-$(document).ready(function (){
+$(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
+
+    countOutputs = [];
+
+    $("li").remove();
 
     var inputNum1 = parseFloat($("#inputNum1").val());
     var inputNum2 = parseFloat($("#inputNum2").val());
@@ -28,23 +31,21 @@ $(document).ready(function (){
       alert("Please enter numbers only.");
     } else {
       if (inputNum1 > 0 && inputNum1 > inputNum2) {
-        alert("Count by must be less than count to!");
+        alert('\"Count By\" must be less than count to!');
       } else if (inputNum1 < 0 && inputNum2 > inputNum1) {
-        alert("Can't count up with a negative!");
+        alert("Can't count up with a negative number!");
       } else if (inputNum1 < 0 && inputNum2 < inputNum1) {
         countDown(inputNum1, inputNum2);
         countOutputs.forEach(function(output) {
-          $("#results #outputs ul").append("<li>" + output + "</li>");
+          $("#results ul").append("<li>" + output + "</li>");
         });
       } else {
         countUp(inputNum1, inputNum2);
         countOutputs.forEach(function(output) {
-          $("#results #outputs ul").append("<li>" + output + "</li>");
+          $("#results ul").append("<li>" + output + "</li>");
         });
       }
     }
-
-    countOutputs = [];
 
   });
 });
