fa012df7e0bf45a57544408648a071de1fae517f
Brea Borlas and Mike Chu <brea@>
2018-03-29 11:48:46 -0700

    Add encoding function for 4-character or less inputs.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,27 @@
+function cryptoSquare(input) {
+  if (input.length < 5) {
+    return makeSquare(input);
+  } else {
+    return input;
+  }
 
+};
+
+function makeSquare(input) {
+  var square = [[],[]];
+  var arraySwitcher = 0;
+  for(var strLength = 0; strLength < input.length; strLength ++) {
+    square[arraySwitcher].push(input[strLength]);
+    if (arraySwitcher === 0){
+      arraySwitcher = 1;
+    }
+    else {
+      arraySwitcher = 0;
+    }
+  }
+  return square.join("").replace(/,/g,"");
+
+};
 
 $(document).ready(function() {
   $("form").submit(function(event) {
