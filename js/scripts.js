3fb974f270d2c00bdfb3e753d13dc2f759ab61f3
Brea Borlas and Mike Chu <mike@>
2018-03-29 08:55:19 -0700

    Add JS to convert numbers 1 - 10.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,27 @@
 var converter = function(input) {
-  return false;
-}
+  var romanNumeral = "";
+  if (input < 4) {
+    for(var index = 0; index < input; index++){
+      romanNumeral += "I";
+    }
+  } else if (input === 4) {
+    romanNumeral += "IV";
+  } else if (input === 5) {
+    romanNumeral += "V";
+  } else if (input < 9) {
+    romanNumeral += "V";
+    for(var index = 0; index < input - 5; index++){
+      romanNumeral += "I";
+    }
+  } else if (input === 9) {
+    romanNumeral += "IX";
+  } else if (input === 10) {
+    romanNumeral += "X";
+  } else {
+      return false;
+  }
+  return romanNumeral;
+};
 
 
 
@@ -9,7 +30,7 @@ var converter = function(input) {
 
 $(document).ready(function() {
   $("#number-form").submit(function(event) {
-    var input = $("#input").val();
+    var input = parseInt($("#input").val());
     $("#result p").text(converter(input));
     event.preventDefault();
   });
