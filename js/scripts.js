10206bb15c41386101540fc8b8fb05402911321a
Brea Borlas and Mike Chu <brea@>
2018-03-29 15:40:51 -0700

    Add crypto square input display.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
+var preEncodeTable = [];
+var squareCounter = 2;
+
 function cryptoSquare(input) {
   var input = input.toLowerCase().replace(/\s/g,"")
-  var squareCounter = 2;
-
+  preEncodeTable = input.split("").slice();
   while (Math.sqrt(input.length) > squareCounter) {
     squareCounter += 1;
   };
-
   return makeSquare(input,squareCounter);
-
 };
 
 function makeSquare(input,squareSize) {
@@ -16,29 +16,24 @@ function makeSquare(input,squareSize) {
   for (var colCounter = 0; colCounter < squareSize; colCounter ++){
     square.push([]);
   }
-  console.log(square);
   var arraySwitcher = 0;
   for(var strLength = 0; strLength < input.length; strLength ++) {
     square[arraySwitcher].push(input[strLength]);
-    if (arraySwitcher  == squareSize - 1){
+    if (arraySwitcher === squareSize - 1){
       arraySwitcher = 0;
     }
     else {
       arraySwitcher += 1;
     }
   }
-
   var preEncodeArray = square.join("").replace(/,/g,"").split("");
-
   while (preEncodeArray.length > 0) {
     for (var i = 0; i < 5; i ++) {
     encodeArray.push(preEncodeArray.shift());
     }
     encodeArray.push(" ");
   }
-
   return encodeArray.join("");
-
 };
 
 $(document).ready(function() {
@@ -47,6 +42,29 @@ $(document).ready(function() {
     var input = $("#input").val();
     var result = cryptoSquare(input);
     $("#result p").text(result);
+    $("#crypto-square").empty();
+    buildCryptoSquare();
+    $("#crypto-square").show();
+    $("#result").show();
   });
 
+  function buildCryptoSquare() {
+    var arraySwitcher = 0;
+    for (var i = 0; i < squareCounter; i ++) {
+      var colName = i+1;
+      $("#crypto-square").append('<div id="col' + colName + '" class="left"></div>');
+    };
+    for (var i = 0; i < preEncodeTable.length; i ++) {
+      var colName = arraySwitcher + 1;
+      var columnID = "#col" + colName;
+      console.log("arraySwitcher");
+      $(columnID).append('<div class="crypto-letter">' + preEncodeTable[i] + '</div>');
+      if (arraySwitcher === squareCounter - 1){
+        arraySwitcher = 0;
+      }
+      else {
+        arraySwitcher += 1;
+      }
+    };
+  };
 });
