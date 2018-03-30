2d2830a8e64fc348876ac58a6303eaaf9ef2bd91
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-03-30 12:38:28 -0700

    Add new form input and function for overwriting default name of Dave.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // BUSINESS LOGIC //
 
 // Primary Beep Boop Function
-function beepyBooper(inputNumber) {
+function beepyBooper(inputNumber, userName) {
   var beepBoopFeedIn = generateNumbers(inputNumber);
   var beepBoopFeedOut = [];
 
@@ -10,7 +10,11 @@ function beepyBooper(inputNumber) {
     var parsedNumber = numberParser(numberString);
 
     if (parseInt(numberString) % 3 === 0 && parseInt(numberString) !== 0) {
-      beepBoopFeedOut.push("I'm sorry, Dave. I'm afraid I can't do that.");
+      if (userName !== "") {
+        beepBoopFeedOut.push("I'm sorry, " + userName + ". I'm afraid I can't do that.");
+      } else {
+        beepBoopFeedOut.push("I'm sorry, Dave. I'm afraid I can't do that.");
+      }
     } else if (parsedNumber.includes("1")) {
       beepBoopFeedOut.push("Boop!");
     } else if (parsedNumber.includes("0")) {
@@ -43,8 +47,9 @@ $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
 
-    var userInput = $("#input").val();
-    var userResult = beepyBooper(userInput);
+    var userInput = $("#input-number").val();
+    var userName = $("#input-name").val();
+    var userResult = beepyBooper(userInput, userName);
 
     $("#result").empty();
 
