c426c03ef15df9675e8fbb4065c61a3298ba57a9
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-04-01 18:56:38 -0700

    Add input validation and error message outputs.

--- js/scripts.js
+++ js/scripts.js
@@ -46,61 +46,80 @@ function numberParser(inputNumber) {
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-    $("h1").fadeOut(200);
-    $("header").slideUp();
-    $("#shield").slideDown();
-
     var userInput = $("#input-number").val();
     var userName = $("#input-name").val();
-    var rawBeepBoop = beepyBooper(userInput, userName);
-    var beepBoopFeedOut = [];
-    $("form")[0].reset();
 
-    // Create HTML Tagged Read-Out
-    rawBeepBoop.map(function(beepBoop) {
-      if (beepBoop === "B00P!") {
-        beepBoopFeedOut.push('<div class="blocks boop">B00P!</div>');
-      } else if (beepBoop === "BEEP!") {
-        beepBoopFeedOut.push('<div class="blocks beep blink' + (Math.floor(Math.random() * 4) + 1) + '">BEEP!</div>');
-      } else if (parseInt(beepBoop) / parseInt(beepBoop) === 1) {
-        beepBoopFeedOut.push('<div class="blocks digit">' + beepBoop + '</div>');
+    // Validate Input
+    if (userInput.match(/\D/g) !== null) {
+      if (userInput.match(/[a-z]/g) !== null) {
+        $("#error-message").html("<span class=\"error\">ERROR: STUPIDITY DETECTED.</span> Human intelligence does not understand data type: number.");
       } else {
-        if (beepBoop === "Dave") {
-          beepBoopFeedOut.push('<div class="blocks sorry">I\'m sorry, Dave. <br>I\'m afraid I can\'t do that.</div>');
-        } else {
-          beepBoopFeedOut.push('<div class="blocks sorry">I\'m sorry, ' + beepBoop + '. <br>I\'m afraid I can\'t do that.</div>');
-        }
+        $("#error-message").html("<span class=\"error\">ERROR: INVALID ID.</span> ID number does not include symbols.");
       }
-    });
+      $("#input-number").val("");
+    } else if (userInput > 1000) {
+      $("#error-message").html("<span class=\"error\">ERROR: Human Overpopulation Imminent.</span> Specimen ID number beyond 1000. Initiating kill sequence.");
+      $("#input-number").val("");
+    // Proceed With Validated Input
+    } else {
+      $("#error-message").html("<span class=\"access\">ACCESS GRANTED</span>");
+      $("#submit").hide();
+      $("h1").fadeOut(200);
+      $("header").slideUp();
+      $("#shield").slideDown();
 
-    // Feed Out HTML Result
-    function resultFeeder(feedOutArray) {
-      $("#result").empty();
-      feedOutArray.forEach(function(number) {
-        $("#result").append(number);
+      var rawBeepBoop = beepyBooper(userInput, userName);
+      var beepBoopFeedOut = [];
+
+      // Create HTML Tagged Read-Out
+      rawBeepBoop.map(function(beepBoop) {
+        if (beepBoop === "B00P!") {
+          beepBoopFeedOut.push('<div class="blocks boop">B00P!</div>');
+        } else if (beepBoop === "BEEP!") {
+          beepBoopFeedOut.push('<div class="blocks beep blink' + (Math.floor(Math.random() * 4) + 1) + '">BEEP!</div>');
+        } else if (parseInt(beepBoop) / parseInt(beepBoop) === 1) {
+          beepBoopFeedOut.push('<div class="blocks digit">' + beepBoop + '</div>');
+        } else {
+          if (beepBoop === "Dave") {
+            beepBoopFeedOut.push('<div class="blocks sorry">I\'m sorry, Dave. <br>I\'m afraid I can\'t do that.</div>');
+          } else {
+            beepBoopFeedOut.push('<div class="blocks sorry">I\'m sorry, ' + beepBoop + '. <br>I\'m afraid I can\'t do that.</div>');
+          }
+        }
       });
-      $("#result").delay(400).fadeIn();
-    };
 
-    resultFeeder(beepBoopFeedOut);
+      // Feed Out HTML Result
+      function resultFeeder(feedOutArray) {
+        $("#result").empty();
+        feedOutArray.forEach(function(number) {
+          $("#result").append(number);
+        });
+        $("#result").delay(400).fadeIn();
+      };
 
-    $("#reverse").show();
-    $("#purge").show();
+      resultFeeder(beepBoopFeedOut);
 
-    // Toggle Feed Direction
-    $(".toggle").click(function() {
-      resultFeeder(beepBoopFeedOut.reverse());
-      $("#reverse").toggle();
-      $("#forward").toggle();
-    });
+      $("#reverse").show();
+      $("#purge").show();
 
-    // Purge Results
-    $("button#purge").click(function() {
-      $("#shield").fadeOut();
-      $("#result").empty().hide();
-      $("button.post-ctrl").hide();
-      $("h1").show();
-      $("header").slideDown();
-    });
+      // Toggle Feed Direction
+      $(".toggle").click(function() {
+        resultFeeder(beepBoopFeedOut.reverse());
+        $("#reverse").toggle();
+        $("#forward").toggle();
+      });
+
+      // Purge Results
+      $("button#purge").click(function() {
+        $("form")[0].reset();
+        $("#shield").fadeOut();
+        $("#error-message").empty();
+        $("#result").empty().hide();
+        $("button.post-ctrl").hide();
+        $("h1").show();
+        $("header").slideDown();
+        $("#submit").fadeIn();
+      });
+    }
   });
 });
