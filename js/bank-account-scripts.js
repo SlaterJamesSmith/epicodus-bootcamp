3c9e2da9dca3b7b4f4601cd5e1bb681be28e3856
Ami Cooper and Mike Chu <mike@>
2018-04-03 15:27:18 -0700

    Add user interface option to register new accounts.

--- js/bank-account-scripts.js
+++ js/bank-account-scripts.js
@@ -10,7 +10,6 @@ var mike = {
   balance: 0
 };
 
-
 var accounts = [mike];
 
 function findAccount(username) {
@@ -26,27 +25,50 @@ function findAccount(username) {
 }
 
 $(document).ready(function() {
-  $("#login").submit(function(event){
+  $(".toggle").click(function() {
+    $("#login").toggle();
+    $("#registration").toggle();
+  });
+
+  // REGISTRATION
+  $("#registration").submit(function(event){
     event.preventDefault();
-    var usernameInput = $("#username").val();
+    console.log(true);
+    var newUsernameInput = $("#new-username").val();
+    var newNameInput = $("#new-name").val();
     var initialDepositInput = 0;
 
+    if ($("#initial-deposit").val() === "") {
+      initialDepositInput = 0;
+    } else {
+      initialDepositInput = parseInt($("#initial-deposit").val());
+    }
+
+    // var Account = findAccount(usernameInput);
+    // if (foundAccount === null) {
+    //   alert("No account found.");
+    // } else {
+    var newAccount = new Account($("#new-username").val(), $("#new-name").val(), parseInt($("#initial-deposit").val()));
+    console.log(newAccount);
+    // accounts.push(newAccount);
+  });
+
+  // LOGIN SECTION
+  $("#login").submit(function(event){
+    event.preventDefault();
+
+    var usernameInput = $("#username").val();
     var foundAccount = findAccount(usernameInput);
     if (foundAccount === null) {
       alert("No account found.");
     } else {
-      if ($("#initial-deposit").val() === "") {
-        initialDepositInput = 0;
-      } else {
-        initialDepositInput = parseInt($("#initial-deposit").val());
-      }
       $("#login").hide();
       $(".transaction").show();
       $(".account-display").show();
       $(".account-name").text(foundAccount[0].name);
-      foundAccount[0].balance += initialDepositInput;
       $(".balance-output").text(foundAccount[0].balance);
 
+      // Transactions
       $(".transaction button").click(function() {
         var deposit = 0;
         var withdraw = 0;
@@ -68,6 +90,7 @@ $(document).ready(function() {
         $(".balance-output").text(foundAccount[0].balance);
       });
 
+      // Logout
       $("#logout").click(function() {
         $("#login").show();
         $(".transaction").hide();
