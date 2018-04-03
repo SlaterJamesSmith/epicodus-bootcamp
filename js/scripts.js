c3d3ad799510a9b2c07d172074b9df76856434d7
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-04-02 23:11:03 -0700

    Refactor user interface output display functions.

--- js/scripts.js
+++ js/scripts.js
@@ -21,9 +21,40 @@ function beepyBooper(inputNumber, userName) {
 
 /// USER INTERFACE LOGIC ///
 $(document).ready(function() {
+
+  function createBeepBoopHtml(inputArray,outputArray) {
+    inputArray.map(function(beepBoop) {
+      if (beepBoop * 0 === 0) {
+        outputArray.push('<div class="blocks digit">' + beepBoop + '</div>');
+      } else if (beepBoop.includes("I'm sorry") === true) {
+        outputArray.push('<div class="blocks sorry strobe' + (Math.floor(Math.random() * 4) + 1) + '">' + beepBoop.slice(0, beepBoop.lastIndexOf(". ") + 2) + '<br>' + beepBoop.slice(beepBoop.lastIndexOf(". ") + 2) + '</div>');
+      } else if (beepBoop === "B00P!") {
+        outputArray.push('<div class="blocks boop pulse' + (Math.floor(Math.random() * 4) + 1) + '">B00P!</div>');
+      } else {
+        outputArray.push('<div class="blocks beep blink' + (Math.floor(Math.random() * 4) + 1) + '">BEEP!</div>');
+      }
+    });
+  }
+
+  function displayBeepBoopResult(array) {
+    $("#result").empty();
+    array.forEach(function(block) {
+      $("#result").append(block);
+    });
+    $(".blocks").click(function() {
+      $(this).remove();
+    });
+    $("#error-message").html("<span class=\"access\">&lt; ACCESS GRANTED &gt;</span>");
+    $("h1").fadeOut(200);
+    $("header").slideUp();
+    $("#shield").slideDown();
+    $("#reverse").show();
+    $("#purge").show();
+    $("#result").delay(400).fadeIn();
+  }
+
   $("form").submit(function(event) {
     event.preventDefault();
-
     var userNumber = $("#input-number").val();
     var userName = $("#input-name").val();
 
@@ -47,47 +78,16 @@ $(document).ready(function() {
       $("#error-message").html("<span class=\"error\">ERROR: Human Breeding Suspected.</span> <br>Specimen ID number beyond 1000. Initiating kill sequence.");
       $("#input-number").val("");
     } else {
-      $("#error-message").html("<span class=\"access\">&lt; ACCESS GRANTED &gt;</span>");
-      $("h1").fadeOut(200);
-      $("header").slideUp();
-      $("#shield").slideDown();
 
       var beepyBooperOutput = beepyBooper(userNumber, userName);
       var beepBoopHtmlFeed = [];
 
-      // Create HTML Tagged Read-Out
-      beepyBooperOutput.map(function(beepBoop) {
-        if (beepBoop * 0 === 0) {
-          beepBoopHtmlFeed.push('<div class="blocks digit">' + beepBoop + '</div>');
-        } else if (beepBoop.includes("I'm sorry") === true) {
-          beepBoopHtmlFeed.push('<div class="blocks sorry strobe' + (Math.floor(Math.random() * 4) + 1) + '">' + beepBoop.slice(0, beepBoop.lastIndexOf(". ") + 2) + '<br>' + beepBoop.slice(beepBoop.lastIndexOf(". ") + 2) + '</div>');
-        } else if (beepBoop === "B00P!") {
-          beepBoopHtmlFeed.push('<div class="blocks boop pulse' + (Math.floor(Math.random() * 4) + 1) + '">B00P!</div>');
-        } else {
-          beepBoopHtmlFeed.push('<div class="blocks beep blink' + (Math.floor(Math.random() * 4) + 1) + '">BEEP!</div>');
-        }
-      });
-
-      // Feed Out HTML Result
-      function resultFeeder(feedOutArray) {
-        $("#result").empty();
-        feedOutArray.forEach(function(number) {
-          $("#result").append(number);
-        });
-        $(".blocks").click(function() {
-          $(this).remove();
-        });
-        $("#result").delay(400).fadeIn();
-      };
-      console.log(beepBoopHtmlFeed);
-      resultFeeder(beepBoopHtmlFeed);
-
-      $("#reverse").show();
-      $("#purge").show();
+      createBeepBoopHtml(beepyBooperOutput,beepBoopHtmlFeed);
+      displayBeepBoopResult(beepBoopHtmlFeed);
 
       // Toggle Feed Direction
       $(".toggle").click(function() {
-        resultFeeder(beepBoopHtmlFeed.reverse());
+        displayBeepBoopResult(beepBoopHtmlFeed.reverse());
         $("#reverse").toggle();
         $("#forward").toggle();
       });
