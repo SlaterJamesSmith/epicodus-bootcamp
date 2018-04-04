06c234ff0124a0be8c7824deb81c7b6376894b73
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-04-04 14:37:12 -0700

    Add functions to update scores and switch player turns.

--- js/pig-dice-scripts.js
+++ js/pig-dice-scripts.js
@@ -2,38 +2,53 @@ function rollDice() {
   return diceValue = Math.floor(Math.random() * 6) + 1;
 }
 
-function endTurn() {
-  return false;
-}
-
 $(document).ready(function() {
   var player1Total = 0;
   var player2Total = 0;
   var player1Pot = 0;
   var player2Pot = 0;
 
+  function switchPlayers() {
+    $(".turn-pot").text("0");
+    $(".buttons").toggle();
+  }
+
   // PLAYER 1 ROLL
-  $(".player1 .roll-dice").click(function() {
+  $("#player1 .roll-dice").click(function() {
     var diceValue = rollDice();
     if (diceValue === 1) {
       player1Pot = 0;
-      endTurn();
+      switchPlayers();
     } else {
       player1Pot += diceValue;
     }
-    $(".player1 .turn-pot").text(player1Pot);
+    $("#player1 .turn-pot").text(player1Pot);
   });
 
   // PLAYER 2 ROLL
-  $(".player2 .roll-dice").click(function() {
+  $("#player2 .roll-dice").click(function() {
     var diceValue = rollDice();
     if (diceValue === 1) {
       player2Pot = 0;
-      endTurn();
+      switchPlayers();
     } else {
       player2Pot += diceValue;
     }
-    $(".player2 .turn-pot").text(player2Pot);
+    $("#player2 .turn-pot").text(player2Pot);
+  });
+
+  // PLAYER 1 HOLD
+  $("#player1 .end-turn").click(function() {
+    player1Total += player1Pot;
+    $("#player1 .total-points").text(player1Total);
+    switchPlayers();
+  });
+
+  // PLAYER 2 HOLD
+  $("#player2 .end-turn").click(function() {
+    player2Total += player2Pot;
+    $("#player2 .total-points").text(player2Total);
+    switchPlayers();
   });
 
 });
