c0bb876a5938278d4f4509d4b448273fe4cb1da6
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-04-06 09:27:23 -0700

    Create PizzaOrder constructor and add function to order one type of pizza.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,19 @@
 /// BUSINESS LOGIC ///
-
+function PizzaOrder() {
+  this.size = "medium";
+  this.toppings = ["pepperoni"];
+  this.price;
+}
 
 /// USER INTERFACE LOGIC ///
 $(document).ready(function() {
+  var pizzaQueue = [];
 
+  $("form#order").submit(function(event) {
+    event.preventDefault();
+    var newPizza = new PizzaOrder();
+    pizzaQueue.push(newPizza);
+
+    console.log(pizzaQueue[0]);
+  });
 });
