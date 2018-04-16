92908eb19e57231170f3f8ad9faa2b51268c0d8e
Mike Chu and William Chu <mike@>
2018-04-16 10:57:45 -0700

    Add random card positions function using Durstenfeld Shuffle Algorithm.

--- memory/src/memory.js
+++ memory/src/memory.js
@@ -11,3 +11,19 @@ Card.prototype.flipUp = function () {
 Card.prototype.flipDown = function () {
   this.faceUp = false;
 };
+
+export function cardPositions() {
+  var deckSize = 12;
+  var positionsArray = [];
+  for (var i = 1; i <= deckSize; i++) {
+    positionsArray.push(i);
+  }
+  // Durstenfeld Shuffle Algorithm
+  for (var i = positionsArray.length -1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = positionsArray[i];
+    positionsArray[i] = positionsArray[j];
+    positionsArray[j] = temp;
+  }
+  return positionsArray;
+}
