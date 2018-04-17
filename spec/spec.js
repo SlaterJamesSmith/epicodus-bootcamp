867dbc90a6302083206abc3f4daa3e6fd2e3194c
Mike Chu and Matt Dowdney <mike@>
2018-04-17 11:32:48 -0700

    Add unit test for generating an array of numbers divisible by 3.

--- spec/spec.js
+++ spec/spec.js
@@ -4,3 +4,12 @@ it('should generate an array of numbers between 1 and 1000', function(){
   var testArray = generateNumbers();
   expect(testArray[999]).toEqual(1000);
 });
+
+it('should generate an array of numbers between 1 and 1000 that are divisible by three', function(){
+  var testArray = generateNumbers();
+  var arraySum = 0;
+  testArray.forEach(function(element) {
+    arraySum += element;
+  });
+  expect(arraySum % 3).toEqual(0);
+});
