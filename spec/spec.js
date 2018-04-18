1b5ca2f819d1315d95f9c7595389b47cd0488559
Cory West-Forbes and Mike Chu <mike@>
2018-04-18 10:47:11 -0700

    Refactor spec tests to include testing character before each test case.

--- spec/spec.js
+++ spec/spec.js
@@ -1,9 +1,13 @@
 import { Character } from './../src/character.js';
 
 describe('Character', function() {
+  var newCharater;
+
+  beforeEach(function() {
+    newCharater = new Character('John', 10, 10, 5);
+  });
 
   it('should create a character with player inputs', function() {
-    var newCharacter = new Character('John', 10, 10, 5);
     expect(newCharacter.name).toEqual('John');
     expect(newCharacter.strength).toEqual(10);
     expect(newCharacter.dexterity).toEqual(10);
@@ -11,9 +15,10 @@ describe('Character', function() {
   });
 
   it('should add starting health and inventory to character', function() {
-    var newCharacter = new Character('John', 10, 10, 5);
     expect(newCharacter.health).toEqual(100);
     expect(newCharacter.inventory).toEqual(['bandage', 'bat']);
   });
+
+  
 
 });
