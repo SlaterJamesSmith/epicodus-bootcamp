1be4b98f8d6fae4e29ad9b29715e88f67c341fff
Cory West-Forbes and Mike Chu <mike@>
2018-04-18 10:40:24 -0700

    Add initially failing unit test for starting health and inventory.

--- spec/spec.js
+++ spec/spec.js
@@ -1,11 +1,19 @@
 import { Character } from './../src/character.js';
 
-describe('Character', function(){
-  it('should create a character with player inputs', function(){
+describe('Character', function() {
+
+  it('should create a character with player inputs', function() {
     var newCharacter = new Character('John', 10, 10, 5);
     expect(newCharacter.name).toEqual('John');
     expect(newCharacter.strength).toEqual(10);
     expect(newCharacter.dexterity).toEqual(10);
     expect(newCharacter.intelligence).toEqual(5);
   });
+
+  it('should add starting health and inventory to character', function() {
+    var newCharacter = new Character('John', 10, 10, 5);
+    expect(newCharacter.health).toEqual(100);
+    expect(newCharacter.inventory).toEqual(['bandage', 'bat']);
+  });
+
 });
--- src/main.js
+++ src/main.js
@@ -4,6 +4,6 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
-$(function(){
+$(function() {
 
 });
