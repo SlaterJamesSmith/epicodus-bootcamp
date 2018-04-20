3fa60da658bea9dfb57c0398c2fd7d9537c91d47
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-04-20 10:55:57 -0700

    Pass test for Earth-age year calculation.

--- src/solar-year.js
+++ src/solar-year.js
@@ -1,5 +1,13 @@
 class User {
+  constructor() {
+    this.birthDate;
+    this.currentDate;
+    this.earthAgeYears;
+  }
 
+  calcEarthAge() {
+    this.earthAgeYears = this.currentDate.getFullYear() - this.birthDate.getFullYear();
+  }
 }
 
 export { User };
