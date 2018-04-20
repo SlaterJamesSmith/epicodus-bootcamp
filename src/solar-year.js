6711c3c8a92818dfb4bbda0f50d5d5cf71245ff5
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-04-20 12:55:32 -0700

    Pass test for calculation of age in Mercury years.

--- src/solar-year.js
+++ src/solar-year.js
@@ -4,11 +4,16 @@ class User {
     this.currentDate;
     this.earthAgeSeconds;
     this.earthAgeYears;
+    this.mercuryAgeYears;
   }
 
   calcEarthAge() {
     this.earthAgeSeconds = (this.currentDate - this.birthDate) / 1000;
     this.earthAgeYears = this.earthAgeSeconds / 31536000;
+  }
+
+  calcSolarYearAge() {
+    this.mercuryAgeYears = parseFloat((this.earthAgeYears / 0.24).toFixed(4));
   }
 }
 
