c567c8a8e563dd1b72686ad8a19d64042e868b8d
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-04-20 12:09:15 -0700

    Rewrite first test to check calculation of Earth-age in seconds instead of years.

--- spec/solar-year-spec.js
+++ spec/solar-year-spec.js
@@ -5,13 +5,13 @@ describe('SolarYearAge', function() {
 
   beforeEach(function() {
     userData = new User();
-    userData.currentDate = new Date(2018, 3, 20);
+    userData.currentDate = new Date(2018, 3, 20, 0, 0, 0);
   });
 
-  it('should calculate user Earth age based on birth year and current year', function() {
-    userData.birthDate = new Date(2017, 3, 20);
+  it('should calculate user Earth-age in seconds based on birth date and current date', function() {
+    userData.birthDate = new Date(2017, 3, 20, 0, 0, 0);
     userData.calcEarthAge();
-    expect(userData.earthAgeYears).toEqual(1);
+    expect(userData.earthAgeSeconds).toEqual(31536000);
   });
 
 });
