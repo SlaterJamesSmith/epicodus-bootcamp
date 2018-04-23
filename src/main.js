0f8572090c041685433743f756343dd04850608d
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-04-22 18:21:04 -0700

    Add JS for reset button and initial form submit function.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,21 @@
 import $ from 'jquery';
-// import { function } from './filename';
+import { User } from './solar-year.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function() {
+  $('form').submit(function(e) {
+    e.preventDefault();
+    let user = new User();
 
+    $('form').hide();
+    $('#results').show();
+    $('#reset').show();
+  });
+
+  $('#reset').click(function() {
+    $('form')[0].reset();
+    location.reload();
+  });
 });
