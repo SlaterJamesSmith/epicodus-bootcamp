aa3699310945798a7427d5139bc728e0ebf851ee
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-04-22 19:06:53 -0700

    Add JS to load and process user input.

--- src/main.js
+++ src/main.js
@@ -8,6 +8,12 @@ $(document).ready(function() {
   $('form').submit(function(e) {
     e.preventDefault();
     let user = new User();
+    user.currentDate = new Date(Date.now());
+    user.birthDate = new Date($('#birthdate').val());
+    user.birthSexOrigin = $('input:radio[name=birth-sex-origin]:checked').val();
+    user.calcEarthAgeData();
+    user.calcEarthLifeExpectancy();
+    user.calcPlanetData();
 
     $('form').hide();
     $('#results').show();
