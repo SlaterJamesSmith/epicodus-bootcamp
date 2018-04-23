99fc8ec7eaf02422553f2c3130b0e003b74af618
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-04-22 23:07:41 -0700

    Add JS to output age calculation results to UI.

--- src/main.js
+++ src/main.js
@@ -12,6 +12,15 @@ $(document).ready(function() {
     user.calcEarthLifeExpectancy();
     user.calcPlanetData();
 
+    $('#earth .age span').text(user.earthAgeYears);
+    $('#earth .life-expectancy span').text(user.lifeExpectancy);
+    $('#earth .years-left span').text(user.earthExpectedYearsLeft);
+    $('#earth .years-past span').text(user.earthYearsPastExpectancy);
+
+    user.planets.forEach(function(planet) {
+      $('#results').append('<div id="' + planet.planet + '"><h3>' + planet.planet + ' Years</h3><p class="age">Age: <span>' + planet.ageYears + '</span></p><p class="years-left">Expected Years Left: <span>' + planet.expectedYearsLeft + '</span></p><p class="years-past">Years Past Life Expectancy: <span>' + planet.yearsPastExpectancy + '</span></p></div>')
+    });
+
     $('form').hide();
     $('#results').show();
     $('#reset').show();
