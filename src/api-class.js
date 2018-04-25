d5b33396aa35109dca9ea35827805820f00e24c0
Mike Chu and Pablo Barreyro <pablo@>
2018-04-25 16:15:42 -0700

    Add UI and earthquake trend plotter function.

--- src/api-class.js
+++ src/api-class.js
@@ -1,9 +1,21 @@
-class API {
-  quakesCall(startTime, endTime, parseResponse){
-
-  }
-  
-  mapsCall() {
-
+class Api {
+  quakesCall(startTime, endTime){
+    let promise;
+    return promise = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=${startTime}&endtime=${endTime}`;
+      request.responseType = 'json';
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open('GET', url, true);
+      request.send();
+    });
   }
 }
+
+export { Api }
