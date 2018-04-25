88e3f34ff646727209895cd43bebf5998b58fdb4
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-04-24 19:20:35 -0700

    Remove Open Weather API and prepare API Call project for repository merge.

---
+++ api-call/src/main.js
@@ -0,0 +1,50 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
+$(document).ready(function() {
+
+  // API Call with jQuery (AJAX)
+  $("#ajax").click(function() {
+    let userInput = $('#user-input').val();
+    $('#user-input').val("");
+
+    $.ajax({
+      url: `http://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${userInput}`,
+      type: 'GET',
+      data: {
+        format: 'json'
+      },
+      success: function(response) {
+        $('#gif-results').append(`<img src="${response.data[0].images.fixed_height.url}">`);
+      },
+      error: function() {
+        $('#errors').text("There was an error processing your request. Please try again.");
+      }
+    });
+  });
+
+  // API Call with Vanilla JavaScript
+  $("#javascript").click(function() {
+    let userInput = $('#user-input').val();
+    $('#user-input').val("");
+
+    let request = new XMLHttpRequest();
+    let url = `http://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${userInput}`;
+
+    request.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        let response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    }
+
+    request.open("GET", url, true);
+    request.send();
+
+    getElements = function(response) {
+      $('#gif-results').append(`<img src="${response.data[0].images.fixed_height.url}">`);
+    }
+  });
+});
