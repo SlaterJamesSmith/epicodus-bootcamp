60266ec28cedd4f506a4a571aaeece421136bf11
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-04-26 07:01:42 -0700

    Update frontend JS to display life expectancies for all planets.

--- src/main.js
+++ src/main.js
@@ -13,12 +13,12 @@ $(document).ready(function() {
     user.calcPlanetData();
 
     $('#earth .age span').text(user.earthAgeYears);
-    $('#earth .life-expectancy span').text(user.lifeExpectancy);
+    $('#earth .life-expectancy span').text(user.earthLifeExpectancy);
     $('#earth .years-left span').text(user.earthExpectedYearsLeft);
     $('#earth .years-past span').text(user.earthYearsPastExpectancy);
 
     user.planets.forEach(function(planet) {
-      $('#results').append('<div id="' + planet.planet + '"><h3>' + planet.planet + ' Years</h3><p class="age">Age: <span>' + planet.ageYears + '</span></p><p class="years-left">Expected Years Left: <span>' + planet.expectedYearsLeft + '</span></p><p class="years-past">Years Past Life Expectancy: <span>' + planet.yearsPastExpectancy + '</span></p></div>');
+      $('#results').append('<div id="' + planet.planet + '"><h3>' + planet.planet + ' Years</h3><p class="age">Age: <span>' + planet.ageYears + '</span></p><p class="life-expectancy">Life Expectancy: <span>' + planet.lifeExpectancy + '</span></p><p class="years-left">Expected Years Left: <span>' + planet.expectedYearsLeft + '</span></p><p class="years-past">Years Past Life Expectancy: <span>' + planet.yearsPastExpectancy + '</span></p></div>');
     });
 
     $('form').hide();
