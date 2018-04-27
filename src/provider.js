c9e950caf7e8ae9b8597d67b22877fa91cc22384
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-04-27 13:50:29 -0700

    Update Provider class constructor.

--- src/provider.js
+++ src/provider.js
@@ -2,12 +2,8 @@ class Provider {
   constructor() {
     this.firstName;
     this.lastName;
-    this.specialties = [];
     this.practices = [];
-    this.address;
-    this.phone;
-    this.url;
-    this.acceptsNewPatients;
+    this.specialties = [];
   }
 }
 
