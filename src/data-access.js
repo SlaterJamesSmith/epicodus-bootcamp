7f3c8d7cc991445209c40099f374a881aec394cf
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-04-27 11:26:24 -0700

    Fix errors in URL string of API call method.

--- src/data-access.js
+++ src/data-access.js
@@ -7,7 +7,7 @@ class DataAccess {
     let promise;
     return promise = new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=location=45.5231%2C-122.6765%2C50&skip=0&limit=100&user_key=${process.env.exports.apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=45.5231,-122.6765,50&skip=0&limit=100&user_key=${process.env.exports.apiKey}`;
       request.responseType = 'json';
       request.onload = function() {
         if (this.status === 200) {
