38ed8dae9eac395e0cf1d04d5abbf88864f21045
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-04-27 11:40:26 -0700

    Add name paramater to API call method.

--- src/data-access.js
+++ src/data-access.js
@@ -3,11 +3,11 @@ class DataAccess {
     console.log("DataAccessTestMessage: CLASS IMPORT SUCCESSFUL");
   }
 
-  apiCallBetterDoctor() {
+  apiCallBetterDoctor(name) {
     let promise;
     return promise = new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=45.5231,-122.6765,50&skip=0&limit=100&user_key=${process.env.exports.apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=45.5231,-122.6765,50&skip=0&limit=100&user_key=${process.env.exports.apiKey}`;
       request.responseType = 'json';
       request.onload = function() {
         if (this.status === 200) {
