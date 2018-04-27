783933dd96fe665e6618c0758b9a92911027b2fd
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-04-27 15:25:23 -0700

    Add UI logic to display message when API call returns 0 results.

--- src/main.js
+++ src/main.js
@@ -15,7 +15,10 @@ $(document).ready(function() {
     let apiCall = dataAccess.apiCallBetterDoctor(name, query);
     apiCall.then(function(response) {
       let providerResults = dataAccess.parseData(response);
-
+      if (providerResults.length === 0) {
+        $('#results').append("No results. Your search criteria did not match any health provider information.");
+      }
+      $('#results').show();
     }, function(error) {
       $('#results').html(`<div class="text-center"><p class="text-danger">${error}.</p><p>We apologize for the inconvenience. </p><p>Your request cannot be processed at this time. Please try again later.</p></div>`);
       $('#results').show();
