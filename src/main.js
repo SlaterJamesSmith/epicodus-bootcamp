2776e5a87ebbaa80dd5d8609ef0353e8245a5a9c
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-04-27 17:00:22 -0700

    Update UI display function to improve output formatting.

--- src/main.js
+++ src/main.js
@@ -11,13 +11,20 @@ function displayProviders(result) {
     provider.specialties.forEach(function(specialty) {
       $('.provider').last().append(`<h4>${specialty.name}</h4><p>${specialty.description}</p>`);
     });
+    $('.provider').last().append('<hr>');
     provider.practices.forEach(function(practice) {
-      $('.provider').last().append(`<p>Location: ${practice.address.street}, ${practice.address.city}, ${practice.address.state} ${practice.address.zip}</p>`);
+      $('.provider').last().append('<div class="location"></div>');
+      if (practice.acceptsNewPatients) {
+        $('.provider .location').last().append('<p class="text-success"><strong>Accepting New Patients</strong></p>');
+      } else {
+        $('.provider .location').last().append('<p class="text-danger"><strong>No New Patients</strong></p>');
+      }
+      $('.provider .location').last().append(`<p>Location: ${practice.address.street}, ${practice.address.city}, ${practice.address.state} ${practice.address.zip}</p>`);
       if (practice.website !== undefined) {
-        $('.provider').last().append(`<p>Website: <a href="${practice.website}" target="_blank">${practice.website}</a></p>`);
+        $('.provider .location').last().append(`<p>Website: <a href="${practice.website}" target="_blank">${practice.website}</a></p>`);
       }
       practice.phones.forEach(function(phone) {
-        $('.provider').last().append(`<p>${phone.type}: ${phone.number}</p>`);
+        $('.provider .location').last().append(`<p>${phone.type}: ${phone.number}</p>`);
       });
     });
   });
@@ -35,7 +42,7 @@ $(document).ready(function() {
     apiCall.then(function(response) {
       let providerResults = dataAccess.parseData(response);
       if (providerResults.length === 0) {
-        $('#results .container').html('<p>No results. Your search criteria did not match any health provider information.</p>');
+        $('#results .container').html('<p class="text-center">No results. Your search criteria did not match any health provider information.</p>');
       } else {
         displayProviders(providerResults);
       }
