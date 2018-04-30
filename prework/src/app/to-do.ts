5c176b32175949c4317aefbf8b515c9d03a272db
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-04-29 22:48:57 -0700

    Add two more tasks in to-do.ts.

--- prework/src/app/to-do.ts
+++ prework/src/app/to-do.ts
@@ -6,4 +6,6 @@ class Task {
 
 var tasks: Task[] = [];
 tasks.push(new Task('Do the dishes.', 'Medium'));
+tasks.push(new Task('Buy chocolate.', 'Low'));
+tasks.push(new Task('Do laundry.', 'High'));
 console.log(tasks);
