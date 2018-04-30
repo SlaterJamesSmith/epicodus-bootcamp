4bbabd4eb384e7baf02b5e76e2b8f3c91e222ea9
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-04-29 22:10:37 -0700

    Add to-do.ts to contain Task class.

--- prework/src/app/typescript-addition.js
+++
@@ -1,7 +0,0 @@
-var findSum = function (firstValue, secondValue) {
-    var sum = firstValue + secondValue;
-    alert('The sum of your two numbers is: ' + sum);
-};
-var firstNumber = parseInt(prompt('Please enter a number.'));
-var secondNumber = parseInt(prompt('Enter another number.'));
-findSum(firstNumber, secondNumber);
