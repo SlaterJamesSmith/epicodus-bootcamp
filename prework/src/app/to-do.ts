54fb7591a0db2f4e90af140fa285af8713476892
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-04-29 22:59:46 -0700

    Add markDone method to Task class.

--- prework/src/app/to-do.ts
+++ prework/src/app/to-do.ts
@@ -2,6 +2,10 @@ class Task {
   done: boolean = false;
 
   constructor(public description: string, public priority: string) {}
+
+  markDone() {
+    this.done = true;
+  }
 }
 
 var tasks: Task[] = [];
