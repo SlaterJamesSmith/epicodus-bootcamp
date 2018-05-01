61e0aecc368476b0c581836e15ba48008ba8eb33
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-05-01 16:46:03 -0700

    Add recipe model and new recipes to root component in Recipe project.

--- recipe/src/app/app.component.ts
+++ recipe/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from './models/recipe.model'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  recipes: Recipe[] = [
+    new Recipe('PB & J Sandwich',['Sliced Bread','Peanut Butter', 'Jelly'],['Spread peanut butter over one slice of bread.', 'Spread jelly over other slice of bread.', 'Combine slices of bread; peanut butter side to jelly side.']),
+    new Recipe('PB & Banana Sandwich',[' Sliced Bread','Peanut Butter', 'Banana'],['Peel and slice banana into 1cm slices.', 'Spread peanut butter over both slices of bread.', 'Place one layer of banana slices over one of the slices of bread.', 'Place other slice of bread on top (peanut butter side down).'])
+  ]
 }
