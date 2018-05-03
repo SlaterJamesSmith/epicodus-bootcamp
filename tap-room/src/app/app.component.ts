948a54824f378918c49afa049425c75c8e12a1b2
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-05-03 12:01:59 -0700

    Add initial keg data to Tap Room root component.

--- tap-room/src/app/app.component.ts
+++ tap-room/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Keg } from './models/keg.model'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  masterKegTap: Keg[] = [
+    new Keg('All Day IPA', 'Founders', 'India Pale Ale', 4.7, 4),
+    new Keg('Breakfast Stout', 'Founders', 'Oatmeal Stout', 8.3, 5),
+    new Keg('Dirty Bastard', 'Founders', 'Scotch Style Ale', 8.5, 4),
+    new Keg('Imperial Stout', 'Founders', 'Imperial Stout', 10.5, 5),
+    new Keg('Porter', 'Founders', 'Porter', 6.5, 4),
+    new Keg('Solid Gold', 'Founders', 'Lager', 4.4, 4),
+  ];
 }
