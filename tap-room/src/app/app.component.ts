9f8ede076c258958b0f342167d73aab31fcc8698
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-05-03 14:45:58 -0700

    Add EditBeerTapComponent and beer tap editing functionality.

--- tap-room/src/app/app.component.ts
+++ tap-room/src/app/app.component.ts
@@ -11,8 +11,18 @@ export class AppComponent {
     new Keg('All Day IPA', 'Founders', 'India Pale Ale', 4.7, 4),
     new Keg('Breakfast Stout', 'Founders', 'Oatmeal Stout', 8.3, 5),
     new Keg('Dirty Bastard', 'Founders', 'Scotch Style Ale', 8.5, 4),
-    new Keg('Imperial Stout', 'Founders', 'Imperial Stout', 10.5, 5),
-    new Keg('Porter', 'Founders', 'Porter', 6.5, 4),
-    new Keg('Solid Gold', 'Founders', 'Lager', 4.4, 4),
+    // new Keg('Imperial Stout', 'Founders', 'Imperial Stout', 10.5, 5),
+    // new Keg('Porter', 'Founders', 'Porter', 6.5, 4),
+    // new Keg('Solid Gold', 'Founders', 'Lager', 4.4, 4)
   ];
+
+  selectedEditTap: Keg = null;
+
+  openEditTap(tap: Keg) {
+    this.selectedEditTap = tap;
+  }
+
+  closeEditTap() {
+    this.selectedEditTap = null;
+  }
 }
--- tap-room/src/app/app.module.ts
+++ tap-room/src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 import { BeerListComponent } from './beer-list/beer-list.component';
+import { EditBeerTapComponent } from './edit-beer-tap/edit-beer-tap.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BeerListComponent
+    BeerListComponent,
+    EditBeerTapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
