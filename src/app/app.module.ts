f9ec95b7151fa63b9245f458b2b65c21dbf30b3f
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-05-04 17:20:40 -0700

    Create BrowseComponent and add to routes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { routing } from './app.routing';
+import { BrowseComponent } from './browse/browse.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrowseComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,6 +1,12 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BrowseComponent } from './browse/browse.component';
 
-const appRoutes: Routes = [];
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: BrowseComponent
+  }
+];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
