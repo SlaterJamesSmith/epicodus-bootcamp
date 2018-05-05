afcc4afa862b9c0d207c1706a5ddd8f9990bb0cb
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-05-04 17:01:55 -0700

    Add navbar to root component and create app.routing.ts.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { routing } from './app.routing';
 
 
 @NgModule({
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
