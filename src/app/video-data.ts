d1463db886be19f40fc8a34e3454697445f91f43
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-05-05 14:32:57 -0700

    Add more locally stored video data.

--- src/app/video-data.ts
+++ src/app/video-data.ts
@@ -3,5 +3,7 @@ import { Video } from './models/video.model';
 export const VIDEOS: Video[] = [
   new Video('Anime Self-Driving Cars', 'Corridor', 'CorridorDigital', '4ZX7HbkdZ_s', 'https://i.ytimg.com/vi/4ZX7HbkdZ_s/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBjMNBVe8bmpjKRTBSAjhNuaIxVzw'),
   new Video('ANIME FIDGET SPINNERS', 'Corridor', 'CorridorDigital', '6kIsEzEmI9w', 'https://i.ytimg.com/vi/6kIsEzEmI9w/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCD0IqMEG5ymLyyEOQnkmo9ftEHIA'),
-  new Video('Mad Max: Roadkill BBQ', 'Corridor', 'CorridorDigital', 'nDG-l5Y9O64', 'https://i.ytimg.com/vi/nDG-l5Y9O64/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBbRk8Gg2uclxxN0_6JBNhWBJa0tA')
+  new Video('Mad Max: Roadkill BBQ', 'Corridor', 'CorridorDigital', 'nDG-l5Y9O64', 'https://i.ytimg.com/vi/nDG-l5Y9O64/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBbRk8Gg2uclxxN0_6JBNhWBJa0tA'),
+  new Video('Blood Dragon: The Cyber War', 'Corridor', 'Corridor Digital', 'dnuJyJBM_J4', 'https://i.ytimg.com/vi/dnuJyJBM_J4/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLABZ_wPwmyLRLManf-LvRNjK_H_3g'),
+  new Video('Brush With Death', 'Corridor', 'Corridor Digital', 'gsyMiTx_CF8', 'https://i.ytimg.com/vi/gsyMiTx_CF8/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBU_Oghot7SCpym2qW9eTw8Y6jYWg')
 ];
