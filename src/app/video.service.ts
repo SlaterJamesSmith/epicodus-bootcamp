b02f32059c051085302a7000b73fc2bf8d9c1f30
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-05-05 08:39:00 -0700

    Add VideoService.

---
+++ src/app/video.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Video } from './models/video.model';
+import { VIDEOS } from './video-data';
+
+@Injectable()
+export class VideoService {
+
+  constructor() { }
+
+  getVideos() {
+    return VIDEOS;
+  }
+
+}
