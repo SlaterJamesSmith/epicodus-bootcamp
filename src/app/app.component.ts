7e5ef1b4bea14e617a1b43272bdb3eb367962baa
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-05-04 23:47:58 -0700

    Add preliminary video data to BrowseComponent.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import { Video } from './models/video.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  
-}
+export class AppComponent { }
--- src/app/browse/browse.component.ts
+++ src/app/browse/browse.component.ts
@@ -7,5 +7,9 @@ import { Video } from '../models/video.model';
   styleUrls: ['./browse.component.css']
 })
 export class BrowseComponent {
-
+  masterVideos: Video[] = [
+    new Video('Anime Self-Driving Cars', 'Corridor', 'CorridorDigital', 'https://www.youtube.com/embed/4ZX7HbkdZ_s'),
+    new Video('ANIME FIDGET SPINNERS', 'Corridor', 'CorridorDigital', 'https://www.youtube.com/embed/6kIsEzEmI9w'),
+    new Video('Mad Max: Roadkill BBQ', 'Corridor', 'CorridorDigital', 'https://www.youtube.com/embed/nDG-l5Y9O64')
+  ];
 }
