49f23a3996e3afd3e1685b95424e36c628f79471
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-05-05 08:51:16 -0700

    Register VideoService in BrowseComponent.

--- src/app/browse/browse.component.ts
+++ src/app/browse/browse.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { Video } from '../models/video.model';
+import { VideoService } from '../video.service';
 
 @Component({
   selector: 'app-browse',
   templateUrl: './browse.component.html',
-  styleUrls: ['./browse.component.css']
+  styleUrls: ['./browse.component.css'],
+  providers: [VideoService]
 })
 export class BrowseComponent {
+
+  constructor(private videoService: VideoService) { }
+
   videos: Video[];
 }
--- src/app/video.service.ts
+++ src/app/video.service.ts
@@ -10,5 +10,4 @@ export class VideoService {
   getVideos() {
     return VIDEOS;
   }
-
 }
