baec0ef02f44343bef021672a77f2250fc1fab93
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-05-06 15:47:00 -0700

    Add methods for carousel positioning and position limits.

--- src/app/browse/browse.component.ts
+++ src/app/browse/browse.component.ts
@@ -12,23 +12,42 @@ import { Router } from '@angular/router';
 
 export class BrowseComponent implements OnInit {
   videos: Video[];
-  offset: number = 0;
+  maxPosition: number;
+  carouselPosition: number = 0;
 
   constructor(private videoService: VideoService, private router: Router) { }
 
   ngOnInit() {
     this.videos = this.videoService.getVideos();
+    this.calcCarouselLimit();
+  }
+
+  calcCarouselLimit() {
+    let carouselWidth: number = document.getElementById("carousel-all").clientWidth;
+    if (carouselWidth < 638) {
+      this.maxPosition = 4;
+    } else if (carouselWidth < 852) {
+      this.maxPosition = 3;
+    } else if (carouselWidth < 1046) {
+      this.maxPosition = 2;
+    } else {
+      this.maxPosition = 1;
+    }
+  }
+
+  getCarouselPosition() {
+    return `carousel-inner carousel-position-${this.carouselPosition}`;
+  }
+
+  scrollLeft() {
+    this.carouselPosition--;
+  }
+
+  scrollRight() {
+    this.carouselPosition++;
   }
 
   loadVideo(clickedVideo: Video) {
     this.router.navigate(['videos', clickedVideo.id]);
-  }
-
-  scrollLeft() {
-    this.offset += 214;
-  }
-
-  scrollRight() {
-    this.offset -= 214;
   }
 }
