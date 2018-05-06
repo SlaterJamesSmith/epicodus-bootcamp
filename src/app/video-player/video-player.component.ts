3f5fde8dac88b7dd7d8a827f50397ddeb0738e1a
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-05-05 17:53:03 -0700

    Add DomSanitizer in VideoPlayer to dynamically build iframe src URL.

--- src/app/video-player/video-player.component.ts
+++ src/app/video-player/video-player.component.ts
@@ -3,6 +3,7 @@ import { Video } from '../models/video.model';
 import { VideoService } from '../video.service';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-video-player',
@@ -14,8 +15,9 @@ export class VideoPlayerComponent implements OnInit {
   videoId: string;
   video: Video;
   iframeHeight: number;
+  embedUrl;
 
-  constructor(private videoService: VideoService, private route: ActivatedRoute, private location: Location) { }
+  constructor(private videoService: VideoService, private route: ActivatedRoute, private location: Location, private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
     this.route.params.forEach((urlParams) => {
@@ -23,6 +25,12 @@ export class VideoPlayerComponent implements OnInit {
     });
     this.video = this.videoService.getVideoById(this.videoId);
     this.iframeHeight = document.getElementById("video-player").clientWidth / 1.775;
+    this.buildEmbedUrl(this.videoId);
+  }
+
+  buildEmbedUrl(id: string) {
+    let nonTrustedUrl = 'https://www.youtube.com/embed/' + id;
+    this.embedUrl = this.sanitizer.bypassSecurityTrustResourceUrl(nonTrustedUrl);
   }
 
   resizeHeight() {
