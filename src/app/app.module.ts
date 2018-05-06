65073521318b4b79dc2927f896c30e47d63ba1aa
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-05-06 09:40:45 -0700

    Setup Firebase imports/exports in AppModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,16 @@ import { AppComponent } from './app.component';
 import { routing } from './app.routing';
 import { BrowseComponent } from './browse/browse.component';
 import { VideoPlayerComponent } from './video-player/video-player.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { masterFirebaseConfig } from './api-keys';
 
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 @NgModule({
   declarations: [
@@ -14,7 +23,9 @@ import { VideoPlayerComponent } from './video-player/video-player.component';
   ],
   imports: [
     BrowserModule,
-    routing
+    routing,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
