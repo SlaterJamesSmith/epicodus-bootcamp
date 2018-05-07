45e195a81dc0e5eabfa91359b071dbff87b32a5a
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-05-06 17:06:25 -0700

    Add event/property bindings to navbar buttons for click effects.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent { }
+export class AppComponent {
+  clicker: string;
+  effect: string;
+
+  activateBtn(button: string) {
+    this.clicker = button;
+    this.effect = 'circle-btn activate';
+  }
+
+  deactivateBtn(button: string) {
+    this.effect = 'circle-btn';
+  }
+
+  checkClicker(button: string) {
+    if (button === this.clicker) {
+      return this.effect;
+    } else {
+      return 'circle-btn';
+    }
+  }
+}
