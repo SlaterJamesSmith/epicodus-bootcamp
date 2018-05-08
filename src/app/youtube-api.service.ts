e790f641b6b66a308cecfd9d739864316833e409
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-05-08 16:18:51 -0700

    Add initial working API call function to YouTubeApiService.

--- src/app/youtube-api.service.ts
+++ src/app/youtube-api.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { youtubeApiConfig } from './api-keys';
 
 @Injectable()
 export class YouTubeApiService {
+  apiCall;
 
   search() {
-    console.log("Made YouTube API Call.");
+    this.apiCall = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=${youtubeApiConfig.apiKey}
+     &part=snippet,statistics&fields=items(id,snippet,statistics)`;
+      request.responseType = 'json';
+      request.onload = function() {
+        if (request.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      };
+      request.open('GET', url, true);
+      request.send();
+    });
+
+    this.apiCall.then(function(response) {
+      console.log(response);
+    }, function(error) {
+      console.log(error);
+    })
   }
 }
