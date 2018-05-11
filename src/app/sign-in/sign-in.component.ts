500a87559924f753cf3ee3f2c9dfb917f05e1b31
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-05-11 10:02:53 -0700

    Add event bind to SignIn template for username input.

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.css']
 })
+
 export class SignInComponent {
+  invalidUser: boolean;
+  user: User;
 
   constructor() { }
+
+  login(userName: string) {
+    if (userName === '') {
+      this.invalidUser = true;
+    } else {
+      this.user = new User(userName);
+      this.invalidUser = false;
+    }
+  }
 }
