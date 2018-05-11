3c118e2fba42a51994a5dc089db0e9227fd0f5d5
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-05-10 22:33:37 -0700

    Create YTVideo model to represent JSON data from YouTube API.

---
+++ src/app/models/ytVideo.model.ts
@@ -0,0 +1,3 @@
+export class YTVideo {
+  constructor(public id: string, public title: string, public description: string, public channelId: string, public channelTitle: string, public thumbnail: string, public duration: string, public publishDate: string, public viewCount: string, public likeCount: string, public dislikeCount: string) { }
+}
