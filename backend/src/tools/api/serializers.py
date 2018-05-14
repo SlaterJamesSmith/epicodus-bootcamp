9214228916464da2a77b930b80019d3fd42767f4
Adrianacmy <adrianamullaney@gmail.com>
2018-05-14 16:11:54 -0700

    add all fields to serializers

--- backend/src/tools/api/serializers.py
+++ backend/src/tools/api/serializers.py
@@ -6,12 +6,12 @@ from tools.models import Tool
 class ToolListSerializer(serializers.ModelSerializer):
   class Meta:
     model = Tool
-    fields = ['pk', 'title', 'imgUrl', 'createDate', 'brand', 'dueDate'];
+    fields = ['pk', 'title', 'imgUrl', 'createDate', 'brand', 'dueDate', 'status', 'late_fine', 'active', 'featured'];
 
  
 
 class ToolDetailSerializer(serializers.ModelSerializer):
    class Meta:
     model = Tool
-    fields = ['pk', 'title', 'imgUrl', 'createDate', 'brand', 'dueDate'];
+    fields = ['pk', 'title', 'imgUrl', 'createDate', 'brand', 'dueDate', 'status', 'late_fine', 'active', 'featured'];
 
