fbd94ac18bb98d20a1f384fe164f97a4e0560c22
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-05-15 09:40:46 -0700

    Update Tool model and add ToolService.

--- client/src/app/dashboard/dashboard.component.ts
+++ client/src/app/dashboard/dashboard.component.ts
@@ -1,19 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../models/user.model';
 import { UserService } from '../user.service';
+import { ToolService } from '../tool.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
-  providers: [UserService]
+  providers: [UserService, ToolService]
 })
 export class DashboardComponent implements OnInit {
   user: User;
+  tools: any[] = [];
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private toolService: ToolService) { }
 
   ngOnInit() {
     this.user = this.userService.getUser();
+    this.toolService.getTools().subscribe(response => {
+      console.log(response);
+    });
   }
 }
