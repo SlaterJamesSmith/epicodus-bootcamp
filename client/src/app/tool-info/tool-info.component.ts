7dd3be4a21e1b58c5d4b14541cb52336504e216f
Adrian Messado <adrian.messado@gmail.com>
2018-05-15 15:51:48 -0700

    add arrow method to ngOnit to retrieve data

--- client/src/app/tool-info/tool-info.component.ts
+++ client/src/app/tool-info/tool-info.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Tool } from '../models/tool.model';
+import { ToolService } from '../tool.service';
+
 
 @Component({
   selector: 'app-tool-info',
   templateUrl: './tool-info.component.html',
-  styleUrls: ['./tool-info.component.css']
+  styleUrls: ['./tool-info.component.css'],
+  providers: [ToolService]
 })
 export class ToolInfoComponent implements OnInit {
-
-  constructor() { }
+private req: any;
+tools: Tool[]=[];
+  constructor( private toolService: ToolService ) { }
 
   ngOnInit() {
-  }
-
+    this.req = this.toolService.getTools().subscribe(data=>{
+    let toolsData=data.json();
+    toolsData.forEach((item)=>{
+      let newTool = new Tool(
+        item.pk,
+        item.title,
+        item.createDate,
+        item.status,
+        item.description,
+        item.brand,
+        item.lateFee,
+        item.active,
+        item.featured,
+        item.dueDate,
+        item.imgUrl
+      );
+      this.tools.push(newTool);
+    });
+  })
+}
 }
