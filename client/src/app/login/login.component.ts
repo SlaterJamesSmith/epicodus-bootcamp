462f3f8ee86ec9055febdfcca16045a0d09588db
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-05-15 12:07:26 -0700

    Add LoginComp template and login functions.

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [UserService]
 })
 
 export class LoginComponent {
+  usernameValid: boolean = null;
 
-  constructor() { }
+  constructor(private userService: UserService) { }
+
+  submitLogin(username, password) {
+    this.validateInput(username);
+    if (this.usernameValid) {
+      this.authenticateLogin(username, password);
+    } else {
+      alert('Login Failed.');
+    }
+  }
+
+  validateInput(username: string) {
+    this.usernameValid = null;
+    if (!username.includes(' ') && username !== '') {
+      this.usernameValid = true;
+    } else {
+      this.usernameValid = false;
+    }
+  }
+
+  authenticateLogin(username: string, password: string) {
+    alert('Login Auth Sent. [CONNECT BACKEND]')
+  }
 }
