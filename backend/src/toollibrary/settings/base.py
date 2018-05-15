d3f03739fc1c4530fd153a719ab87bec1a3fc253
Pablo Barreyro <Pabarreyro@gmail.com>
2018-05-15 14:07:04 -0700

    Add cors-header middleware classes, authorize dev server origin

--- backend/src/toollibrary/settings/base.py
+++ backend/src/toollibrary/settings/base.py
@@ -39,11 +39,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
 
     'tools',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', # Must be first; handles CORS
+    'django.middleware.common.BrokenLinkEmailsMiddleware', # Emails 404 errors
+    'django.middleware.common.CommonMiddleware', # Generates response
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -108,6 +112,16 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+# Cors-header configuration
+# Note: CORS_ORIGIN_REGEX_WHITELIST also available for authorizing origins.
+# https://github.com/ottoyiu/django-cors-headers
+
+CORS_ORIGIN_ALLOW_ALL = False # Always false
+
+CORS_ORIGIN_WHITELIST = ( # Authorize origins here
+    'http//:localhost:8000',
+)
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
