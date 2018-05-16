4c63775a35a097e80db590d5e11f1f666e1f35da
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-05-16 02:03:48 -0700

    Add template and styling for SearchComponent.

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,20 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ActivatedRoute, Params} from '@angular/router';
 import { Location } from '@angular/common';
 import { YTVideo } from '../models/ytvideo.model';
 import { YouTubeApiService } from '../youtube-api.service';
+import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css'],
-  providers: [YouTubeApiService]
+  providers: [YouTubeApiService, AccountService]
 })
 export class SearchComponent implements OnInit {
+  clicker: string;
+  effect: string;
+  videoTarget: string;
+  preventClickOut: boolean = false;
   query: string;
   videos: YTVideo[] = [];
 
-  constructor(private route: ActivatedRoute, private location: Location, private youTubeApiService: YouTubeApiService) { }
+  constructor(private router: Router, private route: ActivatedRoute, private location: Location, private youTubeApiService: YouTubeApiService, private accountService: AccountService) { }
 
   ngOnInit() {
     this.query = this.route.params['_value']['query'];
@@ -26,7 +32,7 @@ export class SearchComponent implements OnInit {
           let video = new YTVideo(
             videoData.id,
             videoData.snippet.localized.title,
-            videoData.snippet.localized.description,
+            videoData.snippet.localized.description.slice(0, 125) + ' ...',
             videoData.snippet.channelId,
             videoData.snippet.channelTitle,
             videoData.snippet.thumbnails.medium.url,
@@ -40,6 +46,83 @@ export class SearchComponent implements OnInit {
         });
       });
     });
-    console.log(this.videos);
+  }
+
+  addToFavorites(video) {
+    this.accountService.addFavoriteVideo(video);
+  }
+
+  addToWatchList(video) {
+    this.accountService.addWatchListVideo(video);
+  }
+
+  channelSubscribe(channel) {
+    this.accountService.channelSubscribe(channel);
+  }
+
+  loadVideo(clickedVideo: YTVideo) {
+    this.router.navigate(['videos', clickedVideo.id]);
+  }
+
+  activateBtn(button: string) {
+    this.clicker = button;
+    this.effect = 'circle-sm activate';
+  }
+
+  deactivateBtn() {
+    setTimeout(() => {
+      this.effect = 'circle-sm deactivate';
+    }, 150);
+  }
+
+  checkClicker(button: string) {
+    if (button === this.clicker) {
+      return this.effect;
+    } else {
+      return 'circle-sm';
+    }
+  }
+
+  clearClicker() {
+    this.clicker = null;
+  }
+
+  optionSelect(video) {
+    if (this.videoTarget === video) {
+      this.videoTarget = null;
+    } else {
+      this.videoTarget = video;
+    }
+  }
+
+  optionsActive(video) {
+    if (this.videoTarget === video) {
+      return 'circle-btn-sm display-flex';
+    } else {
+      return 'circle-btn-sm display-none';
+    }
+  }
+
+  optionsOpen(video) {
+    if (this.videoTarget === video) {
+      return 'options-panel display-flex';
+    } else {
+      return 'options-panel display-none';
+    }
+  }
+
+  protectClick() {
+    this.preventClickOut = true;
+  }
+
+  clickOut() {
+    if (this.preventClickOut === false) {
+      this.videoTarget = null;
+    }
+    this.preventClickOut = false;
+  }
+
+  closeMenu() {
+    this.videoTarget = null;
   }
 }
