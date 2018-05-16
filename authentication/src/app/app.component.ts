638da2d539caf5b684fcda87216f7d1f8701d3be
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-05-15 21:38:50 -0700

    Complete login/logout exercise for Authentication project.

--- authentication/src/app/app.component.ts
+++ authentication/src/app/app.component.ts
@@ -1,9 +1,34 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from './authentication.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AuthenticationService]
 })
 
-export class AppComponent { }
+export class AppComponent {
+  user;
+  private isLoggedIn: boolean;
+  private userName: string;
+
+  constructor(public authService: AuthenticationService) {
+    this.authService.user.subscribe(user => {
+      if (user === null) {
+        this.isLoggedIn = false;
+      } else {
+        this.isLoggedIn = true;
+        this.userName = user.displayName;
+      }
+    });
+  }
+
+  login() {
+    this.authService.login();
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+}
