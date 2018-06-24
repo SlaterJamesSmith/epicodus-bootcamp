10cdeb7e3d8f8b4663bc789af96482d06eb8a287
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-06-24 10:51:05 -0700

    Add live clock to Hello World prework exercise.

--- 01-react-fundamentals/prework-01/src/index.js
+++ 01-react-fundamentals/prework-01/src/index.js
@@ -1,6 +1,12 @@
-const GREETING = React.createElement('h1', {}, 'Hello, World!');
+function liveTime() {
+  const GREETING = React.createElement('h1', {}, 'Hello, World!');
+  const CLOCK = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
+  const APP = React.createElement('div', {}, GREETING, CLOCK);
 
-ReactDOM.render(
-  GREETING,
-  document.getElementById('react-app-root')
-);
+  ReactDOM.render(
+    APP,
+    document.getElementById('react-app-root')
+  );
+}
+
+setInterval(liveTime, 1000);
