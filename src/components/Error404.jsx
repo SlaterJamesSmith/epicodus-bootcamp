76fb0d9f01ff1b0c64f788e8ba023b605cff12be
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-06-29 16:07:17 -0700

    Add styles to Error404 component.

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -3,10 +3,30 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 function Error404(props) {
+  const navLink = {
+    color: 'red',
+    textDecoration: 'none'
+  };
+
   return (
     <div>
+      <style jsx>
+        {`
+          div {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: column;
+            padding: 25px;
+          }
+
+          strong {
+            color: #f00;
+          }
+        `}
+      </style>
       <p><strong>The page "{props.location.pathname}" does not exist.</strong></p>
-      <p>Would you like to return <Link to="/">home</Link> instead?</p>
+      <p>Would you like to return <Link to="/" style={navLink}>home</Link> instead?</p>
     </div>
   );
 }
