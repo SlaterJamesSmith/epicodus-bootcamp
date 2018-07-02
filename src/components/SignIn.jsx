931867549cf556f6762448745bd476b8427a2ab8
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-07-02 00:13:01 -0700

    Add initial HTML/CSS for SignIn component.

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -15,12 +15,55 @@ function SignIn() {
           form {
             height: 500px;
             width: 500px;
+            padding: 50px;
             border: 1px solid #ddd;
+          }
+
+          h2 {
+            font-weight: normal;
+            line-height: 1.5rem;
+          }
+
+          h2 .subtitle {
+            font-size: 1rem;
+            font-weight: 500;
+          }
+
+          label {
+            display: block;
+            color: #4285f4;
+            font-size: 0.8rem;
+          }
+
+          input {
+            width: 100%;
+            display: block;
+            margin-bottom: 25px;
+            padding: 10px;
+            border: none;
+            border-bottom: 2px solid #4285f4;
+            outline: none;
+            font-size: 1rem;
+          }
+
+          button {
+            padding: 10px 20px;
+            border: none;
+            border-radius: 3px;
+            background-color: #4285f4;
+            color: #fff;
+            text-transform: uppercase;
           }
         `}
       </style>
       <form>
-
+        <h2>Sign in<br/>
+          <span className='subtitle'>to continue to YouTube</span></h2>
+        <label>Email</label>
+        <input type='text'/>
+        <label>Password</label>
+        <input type='text'/>
+        <button type='submit'>Next</button>
       </form>
     </div>
   );
