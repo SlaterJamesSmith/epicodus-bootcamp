4b3cceb56e11c528f5a33019c586f2204b437e67
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-07-06 08:45:59 -0700

    Add HTML/CSS for ChannelStrip component.

--- src/components/ChannelStrip.jsx
+++ src/components/ChannelStrip.jsx
@@ -6,16 +6,54 @@ function ChannelStrip(props) {
     <article>
       <style jsx>
         {`
+          .strip-header {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+          }
 
+          .logo-title {
+            display: flex;
+            align-items: center;
+          }
+
+          .logo-title img {
+            width: 32px;
+            margin-right: 10px;
+            border-radius: 100%;
+          }
+
+          h2 {
+            font-size: 1rem;
+            font-weight: 500;
+          }
+
+          .subscribe-btn {
+            padding: 8px 15px;
+            border: none;
+            border-radius: 2px;
+            color: #fff;
+            background-color: #f00;
+            font-family: 'Roboto', sans-serif;
+            font-size: 0.9rem;
+            font-weight: 500;
+            text-transform: uppercase;
+          }
         `}
       </style>
-      <h2>{props.channel.title}</h2>
+      <div className='strip-header'>
+        <div className='logo-title'>
+          <img src={props.channel.thumbnail} alt='channel logo'/>
+          <h2>{props.channel.title}</h2>
+        </div>
+        <button className='subscribe-btn'>Subscribe {props.channel.subscriberCount}</button>
+      </div>
     </article>
   );
 }
 
 ChannelStrip.propTypes = {
   channel: PropTypes.object.isRequired
-}
+};
 
 export default ChannelStrip;
