b346c0cccd30429d581836192a609127ca500980
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-07-08 12:36:01 -0700

    Add second song and restructure initialState for Karaoke project.

--- 03-redux/karaoke/scripts.js
+++ 03-redux/karaoke/scripts.js
@@ -1,10 +1,28 @@
 // LYRIC INFO
-const songLyricsArray = 'Ground Control to Major Tom , Ground Control to Major Tom , Take your protein pills and put your helmet on , Ground Control to Major Tom , Commencing countdown, engines on , Check ignition and may God\'s love be with you , This is Ground Control to Major Tom , You\'ve really made the grade , And the papers want to know whose shirts you wear , Now it\'s time to leave the capsule if you dare , This is Major Tom to Ground Control , I\'m stepping through the door , And I\'m floating in a most peculiar way , And the stars look very different today , For here am I sitting in a tin can , Far above the world , Planet Earth is blue , And there\'s nothing I can do , Though I\'m past one hundred thousand miles , I\'m feeling very still , And I think my spaceship knows which way to go , Tell my wife I love her very much, she knows , Ground Control to Major Tom , Your circuit\'s dead, there\'s something wrong , Can you hear me, Major Tom? , Can you hear me, Major Tom? , Can you hear me, Major Tom? , Can you... , Here am I floating round my tin can , Far above the Moon , Planet Earth is blue , And there\'s nothing I can do.'.split(' , ');
+const songLyricsArray = {
+  1: 'Ground Control to Major Tom , Ground Control to Major Tom , Take your protein pills and put your helmet on , Ground Control to Major Tom , Commencing countdown, engines on , Check ignition and may God\'s love be with you , This is Ground Control to Major Tom , You\'ve really made the grade , And the papers want to know whose shirts you wear , Now it\'s time to leave the capsule if you dare , This is Major Tom to Ground Control , I\'m stepping through the door , And I\'m floating in a most peculiar way , And the stars look very different today , For here am I sitting in a tin can , Far above the world , Planet Earth is blue , And there\'s nothing I can do , Though I\'m past one hundred thousand miles , I\'m feeling very still , And I think my spaceship knows which way to go , Tell my wife I love her very much, she knows , Ground Control to Major Tom , Your circuit\'s dead, there\'s something wrong , Can you hear me, Major Tom? , Can you hear me, Major Tom? , Can you hear me, Major Tom? , Can you... , Here am I floating round my tin can , Far above the Moon , Planet Earth is blue , And there\'s nothing I can do.'.split(' , '),
+  2: 'Stone love , she kneels before the grave , A brave son - who gave his life to save the slogans , That hovers between the headstone and her eyes , For they penetrate her grieving , New love , a boy and girl are talking , New words - that only they can share in , New words , a love so strong it tears their hearts to sleep , through the fleeting hours of morning , Love is careless in its choosing , Sweeping over cross a baby , Love descends on those defenseless , Idiot love will spark the fusion , Inspirations have I none , Just to touch the flaming dove , All I have is my love of love , And love is not loving , Soul love , the priest that tastes the word and , Told of love , and how my God on high is , All love , though reaching up my loneliness evolves , By the blindness that surrounds him , Love descends on those defenseless , Idiot love will spark the fusion , Inspirations have I none , Just to touch the flaming dove , All I have is my love of love , And love is not loving'.split(' , ')
+}
 
 // INITIAL REDUX STATE
 const initialState = {
-  songLyricsArray: songLyricsArray,
-  arrayPosition: 0,
+  currentSongId: null,
+  songsById: {
+    1: {
+      title: 'Space Oddity',
+      artist: 'David Bowie',
+      songId: 1,
+      songArray: songList[1],
+      arrayPosition: 0,
+    },
+    2: {
+      title: 'Soul Love',
+      artist: 'David Bowie',
+      songId: 2,
+      songArray: songList[2],
+      arrayPosition: 0,
+    }
+  }
 };
 
 // REDUX REDUCER
