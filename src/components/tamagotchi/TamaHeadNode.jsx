e9fb20c3a25dacb6cef47ab3c6321c2de10c6b52
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-07-19 14:19:36 -0700

    Add idle animation for TamaHeadNode.

--- src/components/tamagotchi/TamaHeadNode.jsx
+++ src/components/tamagotchi/TamaHeadNode.jsx
@@ -4,7 +4,7 @@ import TamaFaceNode from './TamaFaceNode';
 
 function TamaHeadNode() {
   return (
-    <div className="tama-head-node">
+    <div className="tama-head-node animate-idle">
       <style jsx>
         {`
           .tama-head-node {
@@ -13,6 +13,16 @@ function TamaHeadNode() {
             position: relative;
             display: flex;
             justify-content: center;
+          }
+
+          @keyframes rotate {
+            0% {transform: rotate(15deg);}
+            50% {transform: rotate(-15deg);}
+            100% {transform: rotate(15deg);}
+          }
+
+          .animate-idle {
+            animation: rotate 2s infinite;
           }
         `}
       </style>
