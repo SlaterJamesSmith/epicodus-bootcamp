c8d2fd14028c181a46d8ae31498c3c17406c7d04
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-07-18 18:25:34 -0700

    Add idle animations for TamaHead and TamaMouth.

--- src/components/tamagotchi/TamaHead.jsx
+++ src/components/tamagotchi/TamaHead.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function TamaHead() {
   return (
-    <div className="tama-head">
+    <div className="tama-head animate-idle">
       <style jsx>
         {`
           .tama-head {
@@ -13,6 +13,33 @@ function TamaHead() {
             border-radius: 100%;
             background-color: #f09;
             transition: transform 1s;
+          }
+
+          @keyframes volume-displace {
+            0% {
+              height: 90px;
+              width: 110px;
+            }
+            10% {
+              height: 95px;
+              width: 105px;
+            }
+            50% {
+              height: 100px;
+              width: 100px;
+            }
+            90% {
+              height: 95px;
+              width: 105px;
+            }
+            100% {
+              height: 90px;
+              width: 110px;
+            }
+          }
+
+          .animate-idle {
+            animation: volume-displace 0.8s linear infinite;
           }
         `}
       </style>
--- src/components/tamagotchi/TamaMouth.jsx
+++ src/components/tamagotchi/TamaMouth.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function TamaMouth() {
   return (
-    <div className="tama-mouth">
+    <div className="tama-mouth animate-idle">
       <style jsx>
         {`
           .tama-mouth {
@@ -12,6 +12,20 @@ function TamaMouth() {
             bottom: 20px;
             border-radius: 4px;
             background-color: #fff;
+          }
+
+          @keyframes pucker {
+            0% {width: 20px;}
+            2% {width: 20px;}
+            45% {width: 12px;}
+            50% {width: 10px;}
+            55% {width: 12px;}
+            98% {width: 20px;}
+            100% {width: 20px;}
+          }
+
+          .animate-idle {
+            animation: pucker 0.8s linear infinite;
           }
         `}
       </style>
