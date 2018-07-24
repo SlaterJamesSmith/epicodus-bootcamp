0b2e875463d63bccdb95408845851874a3fe35bc
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-07-24 09:02:42 -0700

    Add animations for Poop component.

--- src/components/tamagotchi/Poop.jsx
+++ src/components/tamagotchi/Poop.jsx
@@ -10,9 +10,11 @@ function Poop() {
             flex-direction: column;
             align-items: center;
             justify-content: center;
+            animation: bounce 0.75s ease-in-out infinite;
           }
 
           .poop-base, .poop-middle, .poop-top {
+            position: relative;
             border-radius: 20px;
             background-color: brown;
             box-shadow: 0 0 4px 2px #0002;
@@ -21,21 +23,47 @@ function Poop() {
           .poop-base {
             height: 20px;
             width: 50px;
-            animation: growth-large 1s infinite;
+            animation: materialize 0.5s forwards, growth-base 0.5s forwards;
           }
 
           .poop-middle {
             height: 14px;
             width: 30px;
             margin-bottom: -2px;
-            animation: materialize 1s, growth-medium 1s;
+            animation: materialize 1s forwards, growth-middle 1s forwards;
           }
 
           .poop-top {
             height: 14px;
             width: 14px;
             margin-bottom: -2px;
-            animation: materialize 1s, growth-small 1s;
+            animation: materialize 1.2s forwards, growth-top 1.2s forwards;
+          }
+
+          @keyframes materialize {
+            0% {box-shadow: 0 0 4px 2px #0000; transform: translateY(-10px);}
+            100% {box-shadow: 0 0 4px 2px #0002; transform: translateY(0px);}
+          }
+
+          @keyframes growth-base {
+            0% {height: 0px; width: 0px;}
+            100% {height: 20px; width: 50px;}
+          }
+
+          @keyframes growth-middle {
+            0% {height: 0px; width: 0px;}
+            100% {height: 14px; width: 30px;}
+          }
+
+          @keyframes growth-top {
+            0% {height: 0px; width: 0px;}
+            100% {height: 14px; width: 14px;}
+          }
+
+          @keyframes bounce {
+            0% {transform: scale(0.95, 1.05);}
+            50% {transform: scale(1.05, 0.95);}
+            100% {transform: scale(0.95, 1.05);}
           }
         `}
       </style>
