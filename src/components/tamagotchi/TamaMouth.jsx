d39d4ec73071c3f73f9016d39cdd534d39190dfd
Mike Chu <32079668+m-chu@users.noreply.github.com>
2018-10-04 00:21:02 -0700

    Add cookie-eating animation for TamaMouth.

--- src/components/tamagotchi/TamaMouth.jsx
+++ src/components/tamagotchi/TamaMouth.jsx
@@ -48,6 +48,7 @@ function TamaMouth(props) {
           .tama-mouth-inner {
             height: 12px;
             width: 100%;
+            position: relative;
             display: flex;
             justify-content: center;
             border-radius: 5px;
@@ -61,13 +62,32 @@ function TamaMouth(props) {
             border-bottom-left-radius: 2px;
             border-bottom-right-radius: 2px;
             background-color: #fff;
+            z-index: 10;
+          }
+
+          .cookie-track {
+            height: 30px;
+            width: 20px;
+            position: absolute;
+            border-radius: 8px;
+            overflow: hidden;
+          }
+
+          .cookie {
+            width: 20px;
+            height: 20px;
+            opacity: 0;
+            position: relative;
+            margin: 0 auto;
+            border-radius: 100%;
+            background: #f1af38;
           }
 
           .vomit {
             height: 0;
             width: 100%;
             position: absolute;
-            border-radius: 10px;
+            border-radius: 8px;
             background-color: #0f09;
           }
 
@@ -96,6 +116,12 @@ function TamaMouth(props) {
             100% {transform: translateY(0);}
           }
 
+          @keyframes cookie {
+            0% {opacity: 0; top: 10px;}
+            10% {opacity: 1;}
+            100% {opacity: 1; top: -25px;}
+          }
+
           @keyframes vomit {
             0% {height: 0;}
             100% {height: 500px;}
@@ -110,6 +136,10 @@ function TamaMouth(props) {
             animation: munch 0.2s infinite;
           }
 
+          .cookie.animate-eating {
+            animation: cookie 1s linear forwards;
+          }
+
           .vomit.animate-expel {
             animation: vomit 0.7s infinite;
           }
@@ -117,6 +147,9 @@ function TamaMouth(props) {
       </style>
       <div className={'tama-mouth-inner animate-' + mouthAnimation} style={mouthExpression}>
         <div className="tooth"></div>
+        <div className="cookie-track">
+          <div className={'cookie animate-' + mouthAnimation}></div>
+        </div>
         <div className={'vomit animate-' + vomitAnimation}></div>
       </div>
     </div>
